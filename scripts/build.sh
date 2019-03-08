e1cb6edd33768eb6998024a8a17952bb2658c1e1
Rico Huijbers <huijbers@amazon.com>
2019-03-08 14:45:55 +0100

    Try to build on Travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,23 +2,9 @@
 set -euo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 
+$scriptdir/build-npm.sh
+
 npm install -g aws-cdk
-
-# Find and build all NPM projects
-for pkgJson in $(find typescript -name package.json | grep -v node_modules); do
-    (
-        cd $(dirname $pkgJson)
-
-        if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
-
-        npm install
-        npm run build
-
-        cp $scriptdir/fake.context.json cdk.context.json
-        cdk synth
-        rm cdk.context.json
-    )
-done
 
 # Find and build all Maven projects
 for pomFile in $(find java -name pom.xml); do
