f7d8a19acda7b72c666edc4dadbab64607f306a0
Rico Huijbers <huijbers@amazon.com>
2019-03-20 10:38:28 +0100

    Update formatting, add link to main README

--- typescript/elasticbeanstalk/elasticbeanstalk-bg-pipeline/index.ts
+++ typescript/elasticbeanstalk/elasticbeanstalk-bg-pipeline/index.ts
@@ -6,61 +6,62 @@ import s3 = require('@aws-cdk/aws-s3');
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);
+    super(scope, id, props);
 
-		//objects for access parameters
-		const node = this.node;
+    //objects for access parameters
+    const node = this.node;
 
-		const blue_env  = node.getContext("blue_env");
-		const green_env = node.getContext("green_env");
-		const app_name  = node.getContext("app_name");
+    const blue_env = node.getContext("blue_env");
+    const green_env = node.getContext("green_env");
+    const app_name = node.getContext("app_name");
 
-		const bucket = new s3.Bucket(this, 'BlueGreenBucket');
+    const bucket = new s3.Bucket(this, 'BlueGreenBucket');
 
-		const handler = new lambda.Function(this, 'BlueGreenLambda', {
-		runtime: lambda.Runtime.Python36,  
-		code: lambda.Code.directory('resources'),
-				handler: 'blue_green.lambda_handler',
-				environment: {
-					BUCKET: bucket.bucketName
-				}
-		});
+    const handler = new lambda.Function(this, 'BlueGreenLambda', {
+      runtime: lambda.Runtime.Python36,
+      code: lambda.Code.directory('resources'),
+      handler: 'blue_green.lambda_handler',
+      environment: {
+        BUCKET: bucket.bucketName
+      }
+    });
 
-		bucket.grantReadWrite(handler.role);
+    bucket.grantReadWrite(handler.role);
 
-		const repo = new cc.Repository(this, 'Repository' ,{
-			repositoryName: 'MyRepositoryName',
-		});
-		
-		const pipeline = new cp.Pipeline(this, 'MyFirstPipeline');
+    const repo = new cc.Repository(this, 'Repository', {
+      repositoryName: 'MyRepositoryName',
+    });
 
-		const sourceStage = pipeline.addStage({
-			name: 'Source'
-		});
+    const pipeline = new cp.Pipeline(this, 'MyFirstPipeline');
 
-		const sourceAction = new cc.PipelineSourceAction({
-			actionName: 'CodeCommit',
-			repository: repo,
-		});
+    const sourceStage = pipeline.addStage({
+      name: 'Source'
+    });
 
-		sourceStage.addAction(sourceAction);
+    const sourceAction = new cc.PipelineSourceAction({
+      actionName: 'CodeCommit',
+      repository: repo,
+    });
 
-
-		const deployStage = pipeline.addStage({
-			name: 'Deploy'
-		});
+    sourceStage.addAction(sourceAction);
 
 
-		const lambdaAction = new lambda.PipelineInvokeAction({
-			actionName: 'InvokeAction',
-			lambda: handler,
-			userParameters: '{"blueEnvironment":"'+blue_env+'","greenEnvironment":"'+green_env+'", "application":"'+app_name+'"}'
-		});
-
-		deployStage.addAction(lambdaAction);
+    const deployStage = pipeline.addStage({
+      name: 'Deploy'
+    });
 
 
-	}}
+    const lambdaAction = new lambda.PipelineInvokeAction({
+      actionName: 'InvokeAction',
+      lambda: handler,
+      userParameters: '{"blueEnvironment":"' + blue_env + '","greenEnvironment":"' + green_env + '", "application":"' + app_name + '"}'
+    });
+
+    deployStage.addAction(lambdaAction);
+
+
+  }
+}
 
 const app = new cdk.App();
 
--- typescript/elasticbeanstalk/elasticbeanstalk-environment/index.ts
+++ typescript/elasticbeanstalk/elasticbeanstalk-environment/index.ts
@@ -5,25 +5,25 @@ import elasticbeanstalk = require('@aws-cdk/aws-elasticbeanstalk');
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);
+    super(scope, id, props);
 
-		//objects for access parameters
-		const node = this.node;
+    //objects for access parameters
+    const node = this.node;
 
-		const platform  = node.getContext("platform");
+    const platform = node.getContext("platform");
 
 
-		const app = new elasticbeanstalk.CfnApplication(this, 'Application' ,{
-			applicationName: "MyApp"
-		});
-	
-		new elasticbeanstalk.CfnEnvironment(this, 'Environment' ,{
-			environmentName: 'MySampleEnvironment',
-			applicationName: app.applicationName,
-			platformArn: platform
-		});
-	
-	}}
+    const app = new elasticbeanstalk.CfnApplication(this, 'Application', {
+      applicationName: "MyApp"
+    });
+
+    new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
+      environmentName: 'MySampleEnvironment',
+      applicationName: app.applicationName,
+      platformArn: platform
+    });
+  }
+}
 
 const app = new cdk.App();
 
