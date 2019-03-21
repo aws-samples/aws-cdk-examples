02b3b08a10d39845d1b29766e6ad27ffb7fb2a6a
Rico Huijbers <huijbers@amazon.com>
2019-03-21 10:04:19 +0100

    Add validation on * dependencies

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -3,6 +3,19 @@ set -euo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 npm install -g aws-cdk
 
+# Make sure that the package.json has * dependencies
+# for the @aws-cdk libraries.
+verify_star_dependencies() {
+    broken=$(grep '@aws-cdk' package.json | grep -v '*' || true)
+    if [[ "$broken" != "" ]]; then
+        echo '================================================='
+        echo ' These @aws-cdk dependencies must depend on version "*"'
+        echo $broken
+        echo '================================================='
+        exit 1
+    fi
+}
+
 # Find and build all NPM projects
 for pkgJson in $(find typescript -name package.json | grep -v node_modules); do
     (
@@ -10,11 +23,13 @@ for pkgJson in $(find typescript -name package.json | grep -v node_modules); do
 
         if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
 
+        verify_star_dependencies
+
         npm install
         npm run build
 
         cp $scriptdir/fake.context.json cdk.context.json
-        cdk synth
+        npx --package aws-cdk cdk synth
         rm cdk.context.json
     )
 done
