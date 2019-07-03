7a2ce64035e327434f9c392129a69af22ccc2451
Mitch Garnaat <mitch@garnaat.com>
2019-07-03 06:09:45 -0700

    Updating Python examples to 0.36.1 (#64)
    * WIP: Updating Python examples to 0.36.1
    
    * Adding some ECS examples
    
    * Adding more ECS examples
    
    * Adding final ECS examples

--- python/ecs/ecs-service-with-task-placement/app.py
+++ python/ecs/ecs-service-with-task-placement/app.py
@@ -2,11 +2,11 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_elasticloadbalancingv2 as elbv2,
-    cdk,
+    core,
 )
 
-app = cdk.App()
-stack = cdk.Stack(app, "aws-ecs-integ-ecs")
+app = core.App()
+stack = core.Stack(app, "aws-ecs-integ-ecs")
 
 # Create a cluster
 vpc = ec2.Vpc(
@@ -37,7 +37,7 @@ container = task_definition.add_container(
 container.add_port_mappings(
     container_port=80,
     host_port=8080,
-    protocol=ecs.Protocol.Tcp
+    protocol=ecs.Protocol.TCP
 )
 
 # Create Service
@@ -46,7 +46,10 @@ service = ecs.Ec2Service(
     cluster=cluster,
     task_definition=task_definition,
 )
-service.place_packed_by(ecs.BinPackResource.Memory)
-service.place_spread_across(ecs.BuiltInAttributes.AVAILABILITY_ZONE)
 
-app.run()
+service.add_placement_strategies(
+    ecs.PlacementStrategy.packed_by(ecs.BinPackResource.MEMORY))
+service.add_placement_strategies(
+    ecs.PlacementStrategy.spread_across(
+        ecs.BuiltInAttributes.AVAILABILITY_ZONE))
+app.synth()
