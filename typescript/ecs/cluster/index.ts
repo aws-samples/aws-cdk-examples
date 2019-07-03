4e8a904b00d17cd4a4cdd34cd63698ea4985a074
Zvika Badalov <zvika@vikartech.com>
2019-07-03 10:18:55 -0400

    added cluster capacity for further investigation

--- typescript/ecs/cluster/index.ts
+++ typescript/ecs/cluster/index.ts
@@ -23,12 +23,12 @@ class ECSCluster extends cdk.Stack {
     /**
      * This lines seem to break the synth with the following error:
      * Error: There is already a Construct with name 'SsmParameterValue:/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id:C96584B6-F00A-464E-AD19-53AFF4B05118.Parameter' in ECSCluster [MyFirstEcsCluster]
-     * Hence, commenting out
+     * This will be investigated and code will be left for now. This will break build however. 
      */
 
-    // cluster.addCapacity('DefaultAutoScalingGroup', {
-    //   instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
-    // });
+    cluster.addCapacity('DefaultAutoScalingGroup', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+    });
   }
 }
 
