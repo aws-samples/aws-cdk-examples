81b4a5dbfaf00cf01cb240726b8769b61a322770
Zvika Badalov <zvika@vikartech.com>
2019-07-03 09:49:12 -0400

    nit cleanup

--- typescript/ecs/ecs-service-with-task-placement/index.ts
+++ typescript/ecs/ecs-service-with-task-placement/index.ts
@@ -10,29 +10,29 @@ const vpc = new ec2.Vpc(stack, 'Vpc', { maxAZs: 2 });
 
 const cluster = new ecs.Cluster(stack, 'EcsCluster', { vpc });
 cluster.addCapacity('DefaultAutoScalingGroup', {
-  instanceType: new ec2.InstanceType('t2.micro')
+  instanceType: new ec2.InstanceType('t2.micro'),
 });
 
 // Create Task Definition with placement constraint
 const taskDefinition = new ecs.Ec2TaskDefinition(stack, 'TaskDef', {
-  placementConstraints: [ ecs.PlacementConstraint.distinctInstances() ]
+  placementConstraints: [ecs.PlacementConstraint.distinctInstances()],
 });
 
 const container = taskDefinition.addContainer('web', {
-  image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+  image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
   memoryLimitMiB: 256,
 });
 
 container.addPortMappings({
   containerPort: 80,
   hostPort: 8080,
-  protocol: ecs.Protocol.TCP
+  protocol: ecs.Protocol.TCP,
 });
 
 // Create Service
-const service = new ecs.Ec2Service(stack, "Service", {
+const service = new ecs.Ec2Service(stack, 'Service', {
   cluster,
-  taskDefinition
+  taskDefinition,
 });
 
 // Specify binpack by memory and spread across availability zone as placement strategies.
