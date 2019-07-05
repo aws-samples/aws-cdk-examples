51b05a6de7a4ba110cfe30945c4fedb840e6f28b
Piradeep Kandasamy <piradeep@amazon.com>
2019-07-05 11:58:57 -0700

    chore(ecs-cluster): remove comment from the ecs cluster example (#67)

--- typescript/ecs/cluster/index.ts
+++ typescript/ecs/cluster/index.ts
@@ -19,13 +19,6 @@ class ECSCluster extends cdk.Stack {
 
     const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
     cluster.addAutoScalingGroup(asg);
-
-    /**
-     * This lines seem to break the synth with the following error:
-     * Error: There is already a Construct with name 'SsmParameterValue:/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id:C96584B6-F00A-464E-AD19-53AFF4B05118.Parameter' in ECSCluster [MyFirstEcsCluster]
-     * This will be investigated and code will be left for now. This will break build however. 
-     */
-
     cluster.addCapacity('DefaultAutoScalingGroup', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
     });
