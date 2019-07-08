0ee3a149c9c35238089e597d247426a50acd89b8
Mitch Garnaat <mitch@garnaat.com>
2019-07-08 08:49:05 -0700

    Updating all Python examples for CDK release 0.38 (#70)

--- python/ecs/ecs-service-with-task-networking/app.py
+++ python/ecs/ecs-service-with-task-networking/app.py
@@ -2,18 +2,18 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_elasticloadbalancingv2 as elbv2,
-    cdk,
+    core,
 )
 
 # Based on https://aws.amazon.com/blogs/compute/introducing-cloud-native-networking-for-ecs-containers/
 
-app = cdk.App()
-stack = cdk.Stack(app, "ec2-service-with-task-networking")
+app = core.App()
+stack = core.Stack(app, "ec2-service-with-task-networking")
 
 # Create a cluster
 vpc = ec2.Vpc(
     stack, "Vpc",
-    max_a_zs=2
+    max_azs=2
 )
 
 cluster = ecs.Cluster(
@@ -26,20 +26,21 @@ cluster.add_capacity("DefaultAutoScalingGroup",
 # Create a task definition with its own elastic network interface
 task_definition = ecs.Ec2TaskDefinition(
     stack, "nginx-awsvpc",
-    network_mode=ecs.NetworkMode.AwsVpc
+    network_mode=ecs.NetworkMode.AWS_VPC,
 )
 
 web_container = task_definition.add_container(
     "nginx",
     image=ecs.ContainerImage.from_registry("nginx:latest"),
     cpu=100,
-    memory_limit_mi_b=256,
+    memory_limit_mib=256,
     essential=True
 )
-web_container.add_port_mappings(
+port_mapping = ecs.PortMapping(
     container_port=80,
-    protocol=ecs.Protocol.Tcp
+    protocol=ecs.Protocol.TCP
 )
+web_container.add_port_mappings(port_mapping)
 
 # Create a security group that allows HTTP traffic on port 80 for our
 # containers without modifying the security group on the instance
@@ -49,8 +50,8 @@ security_group = ec2.SecurityGroup(
     allow_all_outbound=False
 )
 security_group.add_ingress_rule(
-    ec2.AnyIPv4(),
-    ec2.TcpPort(80)
+    ec2.Peer.any_ipv4(),
+    ec2.Port.tcp(80)
 )
 
 # Create the service
@@ -61,4 +62,4 @@ service = ecs.Ec2Service(
     security_group=security_group
 )
 
-app.run()
+app.synth()
