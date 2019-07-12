d60cf57b760db35aa0560b49d861bb126254626e
Adam Ruka <adamruka85@gmail.com>
2019-07-12 05:50:49 -0700

    fix: CDK General Availability (1.0.0) updates (#76)
    Also build all examples in the PR build.

--- scripts/build-java.sh
+++ scripts/build-java.sh
@@ -2,8 +2,12 @@
 set -euo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 
+# install CDK CLI from npm, so that npx can find it later
+cd $scriptdir/../java
+npm install
+
 # Find and build all Maven projects
-for pomFile in $(find java -name pom.xml); do
+for pomFile in $(find $scriptdir/../java -name pom.xml); do
     (
         cd $(dirname $pomFile)
         mvn compile test
