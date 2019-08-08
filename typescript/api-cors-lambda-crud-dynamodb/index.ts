a8627c78f56b71922287d47c8bce56207593bdcc
Andrew Chatfield <hellnation@gmail.com>
2019-08-08 10:34:43 +1000

    sets removal policy on S3 and DynamoDB to DESTROY

--- typescript/api-cors-lambda-crud-dynamodb/index.ts
+++ typescript/api-cors-lambda-crud-dynamodb/index.ts
@@ -12,7 +12,12 @@ export class ApiLambdaCrudDynamoDBStack extends cdk.Stack {
         name: 'itemId',
         type: dynamodb.AttributeType.STRING
       },
-      tableName: 'items'
+      tableName: 'items',
+
+      // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+      // the new table, and it will remain in your account until manually deleted. By setting the policy to 
+      // DESTROY, cdk destroy will delete the table (even if it has data in it)
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
     });
 
     const getOneLambda = new lambda.Function(this, 'getOneItemFunction', {
--- typescript/appsync-graphql-dynamodb/index.ts
+++ typescript/appsync-graphql-dynamodb/index.ts
@@ -51,7 +51,12 @@ export class AppSyncCdkStack extends cdk.Stack {
         type: AttributeType.STRING
       },
       billingMode: BillingMode.PAY_PER_REQUEST,
-      stream: StreamViewType.NEW_IMAGE
+      stream: StreamViewType.NEW_IMAGE,
+
+      // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+      // the new table, and it will remain in your account until manually deleted. By setting the policy to 
+      // DESTROY, cdk destroy will delete the table (even if it has data in it)
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
     });
 
     const itemsTableRole = new Role(this, 'ItemsDynamoDBRole', {
