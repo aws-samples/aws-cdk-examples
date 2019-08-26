605bda1613b360a56515cc35982960bf9d360d26
Brian Hopkins <you@example.com>
2019-08-26 09:36:24 -0400

    feat(vpcflowlogs): added vpc flow logs to python examples

--- python/vpc/vpc-flow-logs/app.py
+++ python/vpc/vpc-flow-logs/app.py
@@ -6,7 +6,6 @@ from aws_cdk import (
 )
 
 class VpcFlowStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
