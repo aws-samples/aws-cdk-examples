62ab40c0368f34a2ed2568bf4576033acc0103c1
Geoff Weinhold <gweinhold@gmail.com>
2019-09-17 02:54:51 -0700

    feat: add C# port of TypeScript 'my-widget-service' (#92)

---
+++ csharp/my-widget-service/src/MyWidgetService/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyWidgetService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null);
+            new MyWidgetServiceStack(app, "MyWidgetServiceStack", new StackProps());
+            app.Synth();
+        }
+    }
+}
