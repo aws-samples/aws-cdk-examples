96ad5a11adba69b33aa06b3e2ec7b57225fed8f9
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-10-25 05:17:43 -0700

    chore: update lambda.Runtime to NODEJS_10_X (#133)
    * chore: update lambda.Runtime to NODEJS_10_X
    
    Node.js 8.x EoL is on 31 Dec 2019 https://endoflife.date/nodejs
    
    * update dependencies

--- typescript/my-widget-service/widget_service.ts
+++ typescript/my-widget-service/widget_service.ts
@@ -44,7 +44,7 @@ export class WidgetService extends cdk.Construct {
     });
 
     const handler = new lambda.Function(this, "WidgetHandler", {
-      runtime: lambda.Runtime.NODEJS_8_10, // So we can use async in widget.js
+      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
       code: lambda.AssetCode.asset("resources"),
       handler: "widgets.main",
       environment: {
