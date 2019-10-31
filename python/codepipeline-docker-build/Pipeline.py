5ce8acc7d7e61bb7ee962b2a833be6d017561909
mounick-mallow <52061259+mounick-mallow@users.noreply.github.com>
2019-10-31 13:30:06 +0530

    Fixing a Typo  "Pipeline"

--- python/codepipeline-docker-build/Pipeline.py
+++ python/codepipeline-docker-build/Pipeline.py
@@ -49,7 +49,7 @@ class Pipeline(core.Stack):
 
         #pipeline param to get the
         pipeline_param = aws_ssm.StringParameter(
-            self, "PPipeline",
+            self, "Pipeline",
             parameter_name=f"{props['namespace']}-pipeline",
             string_value=pipeline.pipeline_name,
             description='cdk pipeline bucket'
