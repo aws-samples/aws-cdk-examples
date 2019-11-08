be241ddb907d522868eeaad9b6dd60bb5ac439f4
Romain Marcadier-Muller <rmuller@amazon.com>
2019-11-08 14:11:34 +0100

    feat: add RandomWriter C# example (#153)

---
+++ csharp/random-writer/src/RandomWriter/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace RandomWriter
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new RandomWriterStack(app, "RandomWriterStack");
+            app.Synth();
+        }
+    }
+}
