e263cb8cfbda7eda2d607c8efedb62e70a6335ca
Vinay Selvaraj <vinas@amazon.com>
2019-11-18 12:31:56 -0500

    Removed unused imports

--- java/ecs/fargate-load-balanced-service/src/main/java/com/amazonaws/cdk/examples/ECSFargateLoadBalancedStack.java
+++ java/ecs/fargate-load-balanced-service/src/main/java/com/amazonaws/cdk/examples/ECSFargateLoadBalancedStack.java
@@ -9,15 +9,9 @@ import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.patterns.NetworkLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.NetworkLoadBalancedFargateServiceProps;
 import software.amazon.awscdk.services.ecs.patterns.NetworkLoadBalancedTaskImageOptions;
-import software.amazon.awscdk.services.iam.User;
-import software.amazon.awscdk.services.iam.UserProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.TopicProps;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
-import software.amazon.awscdk.services.sqs.QueueProps;
 
 public class ECSFargateLoadBalancedStack extends Stack {
+
     public ECSFargateLoadBalancedStack(final Construct parent, final String id) {
         this(parent, id, null);
     }
