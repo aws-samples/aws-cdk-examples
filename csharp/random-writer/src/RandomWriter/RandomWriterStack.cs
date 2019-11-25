e2942188c8b414934b5ab14986c15ec52cc775c0
curry9999 <curry123456789@gmail.com>
2019-11-25 21:52:21 +0900

    Update Lambda Runtime to NODEJS_12_X

--- csharp/random-writer/src/RandomWriter/RandomWriterStack.cs
+++ csharp/random-writer/src/RandomWriter/RandomWriterStack.cs
@@ -39,7 +39,7 @@ namespace RandomWriter
 
             Function = new Function(this, "Lambda", new FunctionProps
             {
-                Runtime = Runtime.NODEJS_10_X,
+                Runtime = Runtime.NODEJS_12_X,
                 Handler = "index.handler",
                 Code = Code.FromAsset("src/RandomWriter/resources"),
                 Environment = new Dictionary<string, string>
