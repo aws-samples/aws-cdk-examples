ce2ab8ea8fc7828cea6f0edc38c23f67362e7702
djiten75 <Junk123$>
2019-11-29 22:47:20 +0000

    Adding back removed files

--- java/api-cors-lambda-crud-dynamodb/lambda/src/main/java/software/amazon/awscdk/examples/lambda/GetOneItem.java
+++ java/api-cors-lambda-crud-dynamodb/lambda/src/main/java/software/amazon/awscdk/examples/lambda/GetOneItem.java
@@ -9,36 +9,39 @@ import software.amazon.awssdk.services.dynamodb.model.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DeleteItem implements RequestHandler<Map<String,Object>, GatewayResponse>{
+public class GetOneItem implements RequestHandler<Map<String,Object>, GatewayResponse>{
 
 
 
     @Override
     public GatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("Inside lambda: getOneItem "+input.getClass()+ " data:"+input);
+        logger.log("Inside software.amazon.awscdk.examples.lambda: getOneItem "+input.getClass()+ " data:"+input);
+
         Map<String, Object> pathParameters = (Map<String, Object>)input.get("pathParameters");
+        String id=(String)pathParameters.get("id");
+        logger.log("Getting data for input parameter:"+id);
+
+        String output = getData(id);
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        String id=(String)pathParameters.get("id");
-        logger.log("updating data for input parameter:"+id);
-        String output = deleteItem(id);
+
         return new GatewayResponse(output, headers, 200);
     }
 
-    private String deleteItem(String id) {
+    private String getData(String id) {
         DynamoDbClient ddb = DynamoDbClient.create();
         String tableName= System.getenv("TABLE_NAME");
         String primaryKey = System.getenv("PRIMARY_KEY");
         Map<String, AttributeValue> tableKey = new HashMap<>();
         tableKey.put(primaryKey, AttributeValue.builder().s(id).build());
-        DeleteItemRequest getItemRequest= DeleteItemRequest.builder()
+        GetItemRequest getItemRequest= GetItemRequest.builder()
                 .key(tableKey)
                 .tableName(tableName)
                 .build();
-        DeleteItemResponse response = ddb.deleteItem(getItemRequest);
+        GetItemResponse response = ddb.getItem(getItemRequest);
         return response.toString();
-     
+
     }
 }
