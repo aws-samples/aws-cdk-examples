1d27148e8db87311465f14aff2b05b8a67b57103
Mitchell Valine <valinm@amazon.com>
2019-12-03 15:22:11 -0800

    Example Formatting
    Formatted new example source code with the google java formatter.

--- java/static-site/src/main/java/software/amazon/awscdk/examples/StaticSiteApp.java
+++ java/static-site/src/main/java/software/amazon/awscdk/examples/StaticSiteApp.java
@@ -1,52 +1,48 @@
 package software.amazon.awscdk.examples;
 
+import java.util.HashMap;
+import java.util.Map;
 import software.amazon.awscdk.core.App;
 import software.amazon.awscdk.core.Environment;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StaticSiteApp extends Stack {
 
-    /**
-     * This stack relies on getting the domain name from CDK context.
-     * Use 'cdk synth -c domain=mystaticsite.com -c subdomain=www'
-     * Or add the following to cdk.json:
-     * {
-     *   "context": {
-     *     "domain": "mystaticsite.com",
-     *     "subdomain": "www"
-     *   }
-     * }
-    **/
-   
-    public StaticSiteApp(App scope, String id, StackProps props) {
-        super(scope, id, props);
+  /**
+   * This stack relies on getting the domain name from CDK context. Use 'cdk synth -c
+   * domain=mystaticsite.com -c subdomain=www' Or add the following to cdk.json: { "context": {
+   * "domain": "mystaticsite.com", "subdomain": "www" } }
+   */
+  public StaticSiteApp(App scope, String id, StackProps props) {
+    super(scope, id, props);
 
-        // Getting domain and subdomain values from Context
+    // Getting domain and subdomain values from Context
 
-        Map<String, Object> domainValues = new HashMap<String, Object>();
-        domainValues.put("domainName", this.getNode().tryGetContext("domain"));
-        domainValues.put("siteSubDomain",this.getNode().tryGetContext("subdomain"));
-        
-        // Call StaticSiteStack to create the stack
-        new StaticSiteStack(this, "MyStaticSite", domainValues);
+    Map<String, Object> domainValues = new HashMap<String, Object>();
+    domainValues.put("domainName", this.getNode().tryGetContext("domain"));
+    domainValues.put("siteSubDomain", this.getNode().tryGetContext("subdomain"));
 
-    }
-    public static void main(final String argv[]) {
+    // Call StaticSiteStack to create the stack
+    new StaticSiteStack(this, "MyStaticSite", domainValues);
+  }
 
-        App app = new App();
+  public static void main(final String argv[]) {
 
-        // Stack must be in us-east-1, because the ACM certificate for a
-        // global CloudFront distribution must be requested in us-east-1.
-        StackProps pr = new StackProps.Builder().env(Environment.builder().region("us-east-1").account(System.getenv("CDK_DEFAULT_ACCOUNT")).build())
-                .build();
-        
-        new StaticSiteApp(app,"MyStaticSite", pr);
-        app.synth();
-        
-      }
+    App app = new App();
+
+    // Stack must be in us-east-1, because the ACM certificate for a
+    // global CloudFront distribution must be requested in us-east-1.
+    StackProps pr =
+        new StackProps.Builder()
+            .env(
+                Environment.builder()
+                    .region("us-east-1")
+                    .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                    .build())
+            .build();
+
+    new StaticSiteApp(app, "MyStaticSite", pr);
+    app.synth();
+  }
 }
-
-
