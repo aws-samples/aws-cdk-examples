70537a3f3e2333501315ee4740ca13fbd8f25ddb
Mitchell Valine <mitchellvaline@gmail.com>
2019-12-04 14:13:05 -0800

    feat: Format Java Code (#199)
    Formats existing java examples using google-java-format. Adds 'style and
    formatting' section to CONTRIBUTING.md regarding java code formatting.
    
    Fixes #198

--- java/lambda-cron/src/test/java/software/amazon/awscdk/examples/TestUtils.java
+++ java/lambda-cron/src/test/java/software/amazon/awscdk/examples/TestUtils.java
@@ -2,33 +2,36 @@ package software.amazon.awscdk.examples;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import software.amazon.awscdk.core.ConstructNode;
-import software.amazon.awscdk.core.Stack;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.StreamSupport;
+import software.amazon.awscdk.core.ConstructNode;
+import software.amazon.awscdk.core.Stack;
 
 public class TestUtils {
-    private static ObjectMapper JSON = new ObjectMapper();
+  private static ObjectMapper JSON = new ObjectMapper();
 
-    static JsonNode fromFileResource(final URL fileResource) throws IOException {
-        return JSON.readTree(fileResource);
-    }
+  static JsonNode fromFileResource(final URL fileResource) throws IOException {
+    return JSON.readTree(fileResource);
+  }
 
-    static JsonNode toCloudFormationJson(final Stack stack) throws IOException {
-        ConstructNode rootNode = stack.getNode().getRoot().getNode();
-        JsonNode n = JSON.valueToTree(ConstructNode.synth(rootNode).getStack(stack.getStackName()).getTemplate());
-        System.out.println(JSON.writerWithDefaultPrettyPrinter().writeValueAsString(n));
-        return n;
-    }
+  static JsonNode toCloudFormationJson(final Stack stack) throws IOException {
+    ConstructNode rootNode = stack.getNode().getRoot().getNode();
+    JsonNode n =
+        JSON.valueToTree(
+            ConstructNode.synth(rootNode).getStack(stack.getStackName()).getTemplate());
+    System.out.println(JSON.writerWithDefaultPrettyPrinter().writeValueAsString(n));
+    return n;
+  }
 
-    static JsonNode getJsonNode(JsonNode stackJson, String type) {
-        Iterable<JsonNode> iterable = stackJson::elements;
-        Optional<JsonNode> maybe = StreamSupport.stream(iterable.spliterator(), false)
-                .filter(jn -> jn.get("Type").textValue().equals(type)).findFirst();
-        return maybe.map(jn -> jn.path("Properties")).orElseThrow(NoSuchElementException::new);
-    }
+  static JsonNode getJsonNode(JsonNode stackJson, String type) {
+    Iterable<JsonNode> iterable = stackJson::elements;
+    Optional<JsonNode> maybe =
+        StreamSupport.stream(iterable.spliterator(), false)
+            .filter(jn -> jn.get("Type").textValue().equals(type))
+            .findFirst();
+    return maybe.map(jn -> jn.path("Properties")).orElseThrow(NoSuchElementException::new);
+  }
 }
