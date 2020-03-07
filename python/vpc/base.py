b1ec6cae795a0240877dd0d121fa29eac8bae3c3
Josh Jaffe <josjaf@gmail.com>
2020-03-07 18:47:10 -0500

    working vpc example

--- python/vpc/base.py
+++ python/vpc/base.py
@@ -7,11 +7,15 @@ from aws_cdk import (
 )
 import itertools
 
+
 class Base(core.Stack):
     def __init__(self, app: core.App, id: str, props, **kwargs) -> None:
         super().__init__(app, id, **kwargs)
         public_private_selection = [aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC),
                                     aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE)]
+        all_subnet_types = [aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC),
+                            aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE),
+                            aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.ISOLATED)]
         vpc = aws_ec2.Vpc(
             self, 'vpc',
             cidr='10.0.0.0/16',
@@ -27,7 +31,7 @@ class Base(core.Stack):
             ],
             gateway_endpoints={
                 'S3': {'service': aws_ec2.GatewayVpcEndpointAwsService.S3,
-                       'subnets': public_private_selection},
+                       'subnets': all_subnet_types},
                 'DynamoDB': {'service': aws_ec2.GatewayVpcEndpointAwsService.DYNAMODB,
                              'subnets': public_private_selection}
             }
@@ -35,7 +39,7 @@ class Base(core.Stack):
         )
 
         vpc_subnets = vpc.public_subnets + vpc.private_subnets + vpc.isolated_subnets
-        # for session managed
+        # for session manager
         ssm_private_link_endpoint = aws_ec2.InterfaceVpcEndpoint(
             self, 'SSMEndpoint',
             vpc=vpc,
@@ -44,10 +48,7 @@ class Base(core.Stack):
             private_dns_enabled=True
         )
 
-
-        included_resources = ["AWS::EC2::Subnet"]
         excluded_resources = ["AWS::EC2::EIP"]
-
         # this will tag most of the resources in the vpc such as vpc, route tables, igw
         core.Tag.add(vpc, 'app_namespace', 'cdk-examples', exclude_resource_types=excluded_resources)
         core.Tag.add(vpc, 'namespace', 'cdk-examples', exclude_resource_types=excluded_resources)
@@ -61,8 +62,7 @@ class Base(core.Stack):
         for i in vpc_subnets_list:
             core.Tag.add(i, 'namespace', props['namespace'])
 
-
-
+        # tag the subnets with their type
         subnet_resource_type = ["AWS::EC2::Subnet"]
         for i in vpc.isolated_subnets:
             core.Tag.add(i, 'SubnetTier', 'Protected')
@@ -73,8 +73,6 @@ class Base(core.Stack):
             # this has issues
         for i in vpc.public_subnets:
             core.Tag.add(i, 'SubnetTier', 'Public', include_resource_types=subnet_resource_type)
-
-        core.Tag.add(vpc, 'namespace', props['namespace'], exclude_resource_types=excluded_resources)
         self.output_props = props.copy()
 
     # pass objects to another stack
