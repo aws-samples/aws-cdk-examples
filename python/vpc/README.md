07d333f6128a2e85239e6fea340d912225dfb101
Josh Jaffe <josjaf@gmail.com>
2020-03-25 17:38:35 -0400

    updating code and reamde per suggestions

--- python/vpc/README.md
+++ python/vpc/README.md
@@ -4,4 +4,10 @@
 * Gateway Endpoints are deployed to all Subnets
 * PrivateLink endpoint for AWS Session Manager, incurs cost
 * 4 Azs, 1 NatGW per AZ for High Availability, if this number is less, CDK will still create the proper routes
-* NatGW incurs cost
+* NAT Gateway is included for each availability zone. NAT Gateway incurs a cost
+    * The value of the NAT Gateway is that it allows instances to connect outbound to the internet and other AWS services, but prevent the internet from initiating a connection with those instances. 
+    * Read More: https://docs.aws.amazon.com/vpc/latest/userguide/vpc-nat-gateway.html
+* A Private Link Endpoint is included as an example, which incurs a cost. 
+    * Private Link endpoints allow connection to AWS Services from subnets which do not have any internet connectivity. 
+    * Read More: https://docs.aws.amazon.com/vpc/latest/userguide/endpoint-service.html
+    
--- python/vpc/base.py
+++ python/vpc/base.py
@@ -22,11 +22,12 @@ class Base(core.Stack):
             max_azs=4,
             enable_dns_hostnames=True,
             enable_dns_support=True,
-            nat_gateways=4,  # default is 1 per az
+            nat_gateways=4,  # default is 1 per az, this enables high availability
             subnet_configuration=[
                 aws_ec2.SubnetConfiguration(name='PUBLIC,', subnet_type=aws_ec2.SubnetType.PUBLIC, cidr_mask=23),
                 aws_ec2.SubnetConfiguration(name='PRIVATE,', subnet_type=aws_ec2.SubnetType.PRIVATE, cidr_mask=23),
-                aws_ec2.SubnetConfiguration(name='ISOLATED,', subnet_type=aws_ec2.SubnetType.ISOLATED, cidr_mask=23)
+                aws_ec2.SubnetConfiguration(name='ISOLATED,', subnet_type=aws_ec2.SubnetType.ISOLATED, cidr_mask=23),
+                aws_ec2.SubnetConfiguration(name='ISOLATED2,', subnet_type=aws_ec2.SubnetType.ISOLATED, cidr_mask=23)
 
             ],
             gateway_endpoints={
@@ -53,12 +54,12 @@ class Base(core.Stack):
         core.Tag.add(vpc, 'app_namespace', 'cdk-examples', exclude_resource_types=excluded_resources)
         core.Tag.add(vpc, 'namespace', 'cdk-examples', exclude_resource_types=excluded_resources)
 
-
         # flatten list of lists
         vpc_subnets_list = [vpc.public_subnets, vpc.private_subnets, vpc.isolated_subnets]
         # flatten the list
         vpc_subnets_list = list(itertools.chain.from_iterable(vpc_subnets_list))
 
+        # tag the subnets with a namespace
         for i in vpc_subnets_list:
             core.Tag.add(i, 'namespace', props['namespace'])
 
@@ -67,12 +68,22 @@ class Base(core.Stack):
         for i in vpc.isolated_subnets:
             core.Tag.add(i, 'SubnetTier', 'Protected')
 
-        # core.Tag.add(i, 'SubnetTier', 'Protected')
         for i in vpc.private_subnets:
             core.Tag.add(i, 'SubnetTier', 'Private')
-            # this has issues
+
+        # avoid errors with EIP resource tagging
         for i in vpc.public_subnets:
             core.Tag.add(i, 'SubnetTier', 'Public', include_resource_types=subnet_resource_type)
+
+        # separate tags for same cdk subnet type, this is optional, but a good example of working with 2 different subnets that CDK sees as the same, (isolate) but we are separating them for different purposes
+        protected_common = vpc.select_subnets(subnets=vpc.isolated_subnets[0:4])
+        protected_management = vpc.select_subnets(subnets=vpc.isolated_subnets[4:8])
+        for i in protected_common.subnets:
+            core.Tag.add(i, 'SubnetType', 'ProtectedCommon')
+
+        for i in protected_management.subnets:
+            core.Tag.add(i, 'SubnetType', 'ProtectedMgmt')
+
         self.output_props = props.copy()
 
     # pass objects to another stack
