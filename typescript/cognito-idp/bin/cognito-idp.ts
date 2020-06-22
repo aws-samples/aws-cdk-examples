7309eaab9f10aa20d0644f3d4bc1c18fe1523c96
Eric Z. Beard <ericzbeard@gmail.com>
2020-06-22 15:20:59 -0700

    Placeholder

--- typescript/cognito-idp/bin/cognito-idp.ts
+++ typescript/cognito-idp/bin/cognito-idp.ts
@@ -4,20 +4,25 @@ import * as cdk from '@aws-cdk/core';
 import { CognitoIdpStack, CognitoIdpStackProps } from '../lib/cognito-idp-stack';
 import * as fs from 'fs-extra';
 
-// For local development, create a copy of config/env.json called config/env-local.json
 
 const app = new cdk.App();
 
+// For local development, create a copy of config/env.json called config/env-local.json
 const localConfigPath = './config/env-local.json';
 if (fs.existsSync(localConfigPath)) {
+
+    // Deploy to your 'local' development account
+    
     // tslint:disable-next-line: no-var-requires
     const localStack = new CognitoIdpStack(app, 'CognitoIdp-Local', require('.' + localConfigPath));
-} 
+} else {
 
-// tslint:disable-next-line: no-var-requires
-// const prodStack = new CognitoIdpStack(app, 'CognitoIdp-Prod', require('../config/env-prod.json'));
+    // Delete this and replace it with environment-specific stacks like prodStack below
+    const placeholderStack = new cdk.Stack(app, 'Placeholder');
 
-// Other stacks like test, gamma, etc would be added here
+    // tslint:disable-next-line: no-var-requires
+    // const prodStack = new CognitoIdpStack(app, 'CognitoIdp-Prod', require('../config/env-prod.json'));
 
+    // Other stacks like test, gamma, etc would be added here
 
-// Need to perform AWS calls for account X, but the current credentials are for Y
+}
