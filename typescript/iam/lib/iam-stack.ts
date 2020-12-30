11a3d21abe7b61709c66c1ffe6f9eb7c5a95a74e
lachlavass-srg <lachlan.vass@superretailgroup.com>
2020-12-30 17:42:26 +1000

    Commented classes and added to stack

--- typescript/iam/lib/iam-stack.ts
+++ typescript/iam/lib/iam-stack.ts
@@ -1,9 +1,33 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import { IamRole } from "./role";
 
+/**
+ * Main IAM Role Stack
+ */
 export class IamStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+
+    // Custom role class with pre-defined policy
+    // See "./role.ts"
+    const role = new IamRole(this, "IAMSampleRole", {
+      assumedBy: new iam.ServicePrincipal("s3.amazonaws.com")
+    });
+
+    // Example of existing policy lookup for permissions boundary
+    const role2 = new IamRole(this, "IAMSampleRole", {
+      assumedBy: new iam.ServicePrincipal("s3.amazonaws.com"),
+      permissionsBoundary: iam.ManagedPolicy.fromManagedPolicyName(scope, "perm-boundary-lookup", "ExistingPolicyName")
+    });
+
+    // Example of policy lookup AFTER initialisation (via .addManagedPolicy())
+    const role3 = new IamRole(this, "IAMSampleRole", {
+      assumedBy: new iam.ServicePrincipal("s3.amazonaws.com"),
+    });
+
+    role3.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("ExistingPolicyName2"));
+
   }
 }
--- typescript/iam/lib/role.ts
+++ typescript/iam/lib/role.ts
@@ -1,7 +1,10 @@
 import * as cdk from "@aws-cdk/core";
 import * as iam from "@aws-cdk/aws-iam";
 
-class IamRole extends iam.Role {
+/**
+ * Represents an AWS IAM Role, with a built-in policy attachment
+ */
+export class IamRole extends iam.Role {
   constructor(scope: cdk.Construct, id: string, props: iam.RoleProps) {
     super(scope, id, props);
     
@@ -13,6 +16,11 @@ class IamRole extends iam.Role {
 
 }
 
+/**
+ * Represents a custom IAM policy, to be attached to IAM roles, users and groups.
+ * Consists of 1 or more statements.
+ * @example Add statements using this.addStatements(new iam.PolicyStatement({}))
+ */
 class IamSamplePolicy extends iam.Policy {
   private S3_SAMPLE_ARN_1 = "EXAMPLE1";
   private S3_SAMPLE_ARN_2 = "EXAMPLE2";
