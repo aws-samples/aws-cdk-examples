41b9aad9c15b18d454fd93edc5935a36d20f160d
Jerry Kindall <jkindall@amazon.com>
2021-03-10 11:00:17 -0800

    Add snippet extraction workflows and scripts

---
+++ .github/extract-snippets/README.md
@@ -0,0 +1,213 @@
+# Extract Snippets for GitHub Actions
+
+Jerry Kindall, Amazon Web Services  
+Last update 10-Mar-2021
+
+## What it is
+
+This is a GitHub Action workflow with supporting scripts that extracts snippets
+from source code files, which can then be used in documentation via include
+directives.  When the code changes, the snippets are automatically extracted
+again, and the documentation will pick them up on the next build.
+
+There are two separate workflows: 
+
+* Extract Snippets from Push (`extract-snippets-push.yaml`): Extracts snippets
+  from the source files in the most recent push; meant to be triggered on push.
+
+* Extract Snippets in Repo (`extract-snippets-all.yaml`): Extracts snippets
+  from all source files in the repo; meant to be run manually or on a schedule.
+
+As a practical matter, extracting from the entire repo is not noticeably slower
+than extracting from only the files in a push (the git commands take the lion's
+share of either workflow's runtime), though this might become less true as the
+repository grows.
+
+## extract-snippets.sh
+
+This `bash` script calls the Python script (described next) to extract the
+snippets, then checks the results in to the `snippets` branch of the repo. This
+script's arguments are taken as a `bash` command that gathers the files to be
+examined for snippets; the output of that command should be a list of paths,
+one per line.  This list is piped to the Python script on standard input.
+
+For example, the following invocation of the script will pipe the output of
+`ls` into the Python script to extract snippets from the source files in the
+current directory.
+
+`bash extract-snippets.sh ls`
+
+## extract-snippets.py
+
+This script reads from standard input the names of the files containing the
+snippets to be extracted.  It ignores non-source files, hidden files, and files
+in hidden directories (it is not necessary to filter these out beforehand).
+The script's required argument is the directory that the snippets should be
+extracted into.
+
+For example, the following command runs the script on source files in the
+current directory, extracting snippets also into the current directory.
+
+`ls | python3 extract-snippets.py .`
+
+Both Windows and Linux-style paths are supported (they are converted to Linux-
+style paths internally).
+
+The supported source file formats are stored in `snippet-extensions.yml`, which
+contains a map of filename extensions to comment markers.  If a language
+supports more than one line comment marker (I'm looking at you, PHP), you can
+provide them separated by whitespace in a single string:
+
+`php "# //"`
+
+If a language does not support a line comment marker (e.g. C), you can use the
+starting block comment marker.  However, snippet tags must have the closing
+block comment marker on the same line, e.g.:
+
+`/* snippet-start:[terry.riley.in-c] */`
+
+Some languages support both line and block comments.  In this case, we suggest
+you always use the line comment marker for snippet tags.
+
+You may pass a different YAML (or JSON) file as the script's second argument --
+for example, the provided `snippet-extensions-more.yml`, which contains a more
+extensive map of source formats.  Note that if you specify only a filename, the
+file of that name in the same directory as the script (not in the working
+directory!) is used.  To specify a file in the current directory, use `./`,
+e.g. `./my-snippet-extensions.yml`.
+
+The keys in `snippet-extensions.yml` are matched case-sensitively at the end of
+full paths, and can be used to match more than extensions.  If you wanted to
+extract snippets from makefiles, for example, you could add to the mapping:
+
+`/makefile: "#"`
+
+If a given key could match more than one language, the first one listed in the
+extension file wins.
+
+To match all files, use `""` as the key, since there's an empty string at the
+end of every path.  You probably don't want to do this, but you can, perhaps
+in combination with excluding certain files as described next.
+
+To exclude a specific file from being processed, specify the end of its path
+and an empty string as the comment marker.  Such items should appear earlier
+in the file than others that might match, since the first match wins.
+
+`"/lambda/widgets.js": ""`
+
+The output of `extract-snippets.py` is a list of the source files processed.
+Indented under each source file is a list of the snippets extracted from it, if
+any, marked with `W`, `A` (for "write" or "append") or `X` for a duplicate
+snippet that has already been extracted.  At the end of the run, a summary line
+displays the number of unique snippets extracted and the number of source files
+examined.
+
+## Snippet tags
+
+Snippet tags are special single-line comments in source files.  They must not
+follow any code on their line and must begin with the language's single-line
+comment marker (`//` in many languages, `#` in some others).  If a language
+does not have a single-line comment marker, the block comment delimiter may be
+used, but must be closed on the same line.  The snippet tag is followed by the
+snippet directive, a colon, and an argument in square brackets.  Whitespace is
+permitted (but optional) between the comment marker and the snippet directive.
+For example:
+
+`// snippet-start:[cdk.typescript.widget_service]`
+
+Here, the directive begins the extraction of a code snippet to the filename
+specified, with a `.txt` extension.
+
+The main tags used in our repos are `snippet-start` and `snippet-end`.  Each
+`snippet-start` requires a matching `snippet-end` (specifying the same snippet
+name) in the same source file.  Multiple snippets may be extracted from one
+source file, and these may overlap.  Snippet tags do not appear in the
+extracted snippets.
+
+The following two tags are unique to this extractor (they are not supported by
+the original snippet extractor used in the AWS SDK Examples repo).
+
+* `snippet-append`: Extracts additional source code to a snippet file that has
+  already been created by a previous `snippet-start` directive, stopping at
+  `snippet-end` as with `snippet-start`.
+
+* `snippet-echo`: Writes the argument literally to the snippet(s) currently
+  being extracted.  Useful for adding closing braces etc. when extracting part
+  of a code block.  Whitespace is stripped from the right of the argument but
+  not the left, so you can match indentation.
+
+Unique to this extractor, `snippet-start` supports an optional number following
+the closing bracket.  If this number is present, that many spaces are removed
+from the beginning of each line of the snippet, allowing for snippets to be
+dedented (have indentation removed), so their left margin is decreased.  Each
+snippet (even overlapping snippets) has its own dedent level.  If you use
+`snippet-append`, it uses the same dedent specified on `snippet-start`.  Dedent
+does not affect `snippet-echo` (provide the desired indentation yourself).
+
+This extractor also recognizes the following tags (i.e. they are not errors),
+but does not do anything with them.  They are supported for compatibility with
+source files tagged for the AWS SDK Examples repo extractor, which can register
+metadata about each snippet.
+
+* `snippet-keyword`
+* `snippet-service`
+* `snippet-sourceauthor`
+* `snippet-sourcedate`
+* `snippet-sourcedescription`
+* `snippet-sourcesyntax`
+* `snippet-sourcetype`
+
+## Errors
+
+The following situations are errors.
+
+* Unrecognized snippet tag (see preceding section for supported tags).
+
+* Text decoding error.  By default, source files are assumed to be in ASCII. To
+  change the encoding used, sent the environment variable `SOURCE_ENCODING` to
+  `utf8` or another encoding.  Use the Python name, which you can find here:
+
+  https://docs.python.org/3/library/codecs.html#standard-encodings
+
+  Generally you'd do this in the action file, not in the `bash` script.  Like:
+
+```yaml
+    # goes under the `steps` key
+    env:
+        SOURCE_ENCODING: utf8
+```
+
+* `snippet-start` for a snippet file that has already been extracted, *unless*
+  the source file has the same filename and contains exactly the same code.
+  This behavior supports multiple examples that contain the same source code
+  for an incorporated Lambda function or other asset, where that code contains
+  snippets that are extracted.
+
+* `snippet-end` with no corresponding `snippet-start` or `snippet-append` in
+  the same source file.
+
+* Missing `snippet-end` corresponding to a `snippet-start` or `snippet-append`.
+
+* `snippet-append` with no corresponding `snippet-start` in the same source
+  file (you can only append to snippets created in the current source file).
+
+* `snippet-echo` outside of a snippet.
+
+* Invalid integer following `snippet-start` or `snippet-append`.
+
+* Insufficient whitespace at the beginning of a line to dedent it as requested.
+
+* Any snippet contains a tab character (ASCII 9), as indenting by tab is not
+  supported in documentation.
+
+All errors stop processing, and the entire workflow fails.  Any snippets
+extracted before the error are not checked in to the repo.
+
+# README-SNIPPETS.txt
+
+This text file is copied into the snippets directory and should provide
+information that users of the snippets should know.
+
+# Version history
+
+* v.1.0.0 - Initial public release.
---
+++ .github/extract-snippets/extract-snippets.py
@@ -0,0 +1,249 @@
+##!/bin/python3
+
+# extract-snippets.py v1.0.0
+# Jerry Kindall, Amazon Web Services
+
+# extracts marked regions from source files and writes them to a snippets directory.
+# reads list of paths from stdin and extracts snippets from these files.  Takes the
+# directory to which snippets should be extracted as a command line argument.  The
+# second command line argument is optional and specifies the YAML file that contains
+# a map of filename extensions to comment markers (default: snippet-extensions.yml
+# in the same directory as this script)
+
+# examples: 
+#
+#       extract snippets from last commit on current git branch to /tmp
+#           git diff @^ --name-only | python3 extract-snippets.py /tmp
+#
+#       extract snippets from all files to specified directory
+#           find . -type f | python3 extract-snippets.py /path/to/snippets/dir
+#
+#       extract snippets from all files in current dir to current dir,
+#       specifying a different filename map
+#           ls | python3 extract-snippets.py . snippet-extensions-more.yml
+
+# The same snippet can be extracted from more than one source file ONLY if all
+# source files containing the snippet have the same filename and contents.  
+# this is to support e.g. Lambda functions deployed by a CDK app, where the
+# CDK app is provided in multiple languages but the same Lambda function source
+# code (snippet tags included) is used in each version.  Ideally the snippet tags
+# would be removed from the copies of the Lambda source files... ideally.
+
+import sys, os, yaml, re, functools
+
+# all open() calls have an implied encoding parameter
+open = functools.partial(__builtins__.open, 
+    encoding=os.environ.get("SOURCE_ENCODING", "utf8"))
+
+# some constants to make our lives easier
+TAB = "\t"
+EOL = "\n"
+
+# regular expression for matching dedent: 1 or 2 digits
+DIGITS = re.compile("[0-9][0-9]?")
+
+# returns cached contents of a file if it exists, or reads it into the cache and
+# returns it if not.  cache is stored as a default parameter value.
+#
+# the cache is used only when there are duplicate snippets in two or more source files.
+# only one copy of the file is ever cached (the first one that was found) so this shouldn't
+# run up memory too much if you don't have many duplicate snippets.
+def cached(path, cache={}):
+    if path not in cache:
+        with open(path) as infile:
+            cache[path] = infile.read()
+    return cache[path]
+
+# our exception class to make error messages clearer
+class SnipperError(Exception):
+    pass
+
+# a file-like object used to avoid writing duplicate snippets we've already extracted
+class DummyFile:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def write(self, text):
+        pass
+
+    def close(self):
+        pass
+
+# the class that does the snippet extraction. instantiate it passing the directory to
+# which snippets should be extracted.  call the instance with each source file from
+# which snippets should be extracted.
+class Snipper:
+
+    # initialize Snipper
+    def __init__(self, snippetdir):
+        self.dir        = snippetdir
+        self.source     = {}
+        self.count      = 0
+ 
+    # extract snippets from a single file
+    # example snippet tag: // snippet-start:[snippet-name] 8
+    def __call__(self, path):
+        print(path)
+        self.started    = set()         # snippets we've started in this source file
+        self.duplicates = set()         # snippets we've determined are duplicates so we won't append/echo
+        ext = next(e for e in EXTENSIONS if path.endswith(e))       # get first matching filename extension
+        if not COMMENTS[ext]:           # if empty comment marker, do not process snippets in this file
+            print("    not processed")
+            return
+        tag = re.compile(f" *({'|'.join(COMMENTS[ext].split())}) ?snippet-") # e.g. if ext is "// #" end up with regex: " *(#|//) ?snippet-"
+        self.files  = {}                # files currently open to write snippets
+        self.dedent = {}                # amount of whitespace to strip from each line of snippet
+        self.path = path                # source file we are working with (store it on instance so we can use it in error messages)
+        with open(path) as infile:      # read source file entirely into memory
+            self.text = infile.read().rstrip()
+        # process each line in source file. self.i is the line we're on (for error messages)
+        for self.i, self.line in enumerate(self.text.splitlines(keepends=False), start=1):
+            line = self.line            # use a local variable for a bit more performance
+            if tag.match(line):         # line is a snippet directive, parse and process it
+                self.directive = line.split("snippet-")[1].split(":")[0].rstrip()   # get e.g. append fron snippet-append
+                self.arg = line.split("[")[1].split("]")[0].rstrip()                # get e.g. snippet-name from [snippet-name]
+                getattr(self, self.directive.lstrip("_"))(self.arg)     # call our method named same as directive (e.g. start(..) for snippet-start)
+            else:                       # line is NOT a snippet directive. write it to any open snippet files
+                for snip, file in self.files.items():           # for each snippet we're writing, write the line
+                    if TAB in line:                             # our doc systems don't indent tabs consistently, so ban tab
+                        raise SnipperError("tab found in snippet %s at line %s in %s" % self._where)
+                    dedent = self.dedent[snip]
+                    if dedent and line[:dedent].strip():        # is the text we want to strip to dedent all whitespace? error if not 
+                        raise SnipperError(("unable to dedent %s space(s) " % dedent) + 
+                            ("in snippet %s at line %s in %s" % self._where) + 
+                            f"(only indented {len(line) - len(line.lstrip())})")
+                    file.write(line[dedent:].rstrip() + EOL)    # write it (strip whitespace at end just to be neat)
+        # done processing this file. make sure all snippets had snippet-end tags
+        if self.files:
+            raise SnipperError("snippet-end tag(s) for %s missing in %s" % (" ".join(self.files), path))
+
+    # directive: beginning of snippet
+    def start(self, arg):
+        path = os.path.join(self.dir, f"{arg}.txt")
+        indicator = "W"
+        opener = open
+        if os.path.isfile(path):
+            # if snippet output already exists, this is OK only if it is being taken
+            # from a source file of the same name with identical content
+            if self.path.rpartition("/")[2] != self.source[arg].rpartition("/")[2] or self.text != cached(self.source[arg]):
+                raise SnipperError(("duplicate snippet %s at line %s in %s" % self._where) +
+                    " (originally defined in %s)" % self.source[arg])
+            else:
+                indicator = "X"         # show thtis is a duplicate
+                opener = DummyFile      # don't write to this file
+                self.duplicates.add(arg)
+        else:
+            self.count += 1
+        if arg not in self.files:
+            # parse number at end of line as dedent value
+            self.dedent[arg] = int(DIGITS.search(self.line.rpartition("]")[2] + " 0").group(0))
+            self.files[arg] = opener(path, "w")     # open real file or dummy
+            self.started.add(arg)       # record that we started this snippet in this source file
+            if arg not in self.source:  # record that we *first* saw this snippet in this source file
+                self.source[arg] = self.path
+            print("   ", indicator, arg)
+        else:
+            raise SnipperError("snippet file %s is already open at line %s in %s" % self._where)
+
+    # directive: append to given file (for extracting multiple chunks of code to a single snippet)
+    def append(self, arg):
+        if arg not in self.started:     # did we start this snippet in current source file?
+            raise SnipperError("snippet file %s not found at line %s in %s" % self._where)
+        if arg in self.files:           # is the file already open?
+            raise SnipperError("snippet %s is already open at line %s in %s" % self._where)
+        self.files[arg] = DummyFile() if arg in self.duplicates else open(os.path.join(self.dir, arg), "a")
+        print("   A", arg)
+
+    # directive: end of snippet
+    def end(self, arg):
+        if arg in self.files:
+            self.files[arg].close()
+            del self.files[arg]
+        else:
+            raise SnipperError("snippet file %s not open at %s in %s" % self._where)
+
+    # directive: insert arg as a line into all currently open snippets (useful for e.g. adding closing brackets to partial snippet)
+    def echo(self, arg):
+        arg = arg.rstrip() + EOL
+        if self.files:
+            for file in self.files.values():
+                file.write(arg)
+        else:
+            raise SnipperError("echo '%s' outside snippet at %s in %s" % self._where)
+
+    # do-nothing handler used for directives that we ignore
+    def _nop(self, arg): return
+
+    # the aforementioned ignored directives
+    service = comment = keyword = sourceauthor = sourcedate = sourcedescription = sourcetype = _nop
+
+    # convenience property for returning error location tuple (used in error messages)
+    @property
+    def _where(self):
+        return self.arg, self.i, self.path
+
+    # called when there's no method on this class to handle a directive, which is an error
+    def __getattr__(self, name):
+        raise SnipperError("invalid directive snippet-%s at %s in %s" % (self.directive, self.i, self.path))
+
+# ----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    # get output directory from command line, or error
+    if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
+        snippetdir = sys.argv[1]
+    else:
+        raise FileNotFoundError("snippet output directory not passed or does not exist")
+
+    # get filename of extersions list from command line, or use default, then load it
+    if len(sys.argv) > 2:
+        commentfile = sys.argv[2]
+    else:
+        commentfile = "snippet-extensions.yml"
+    # if no directory specified, file is in same directory as script
+    if "/" not in commentfile and "\\" not in commentfile:
+        commentfile = os.path.join(os.path.dirname(__file__), commentfile)
+    if not os.path.isfile(commentfile):
+        raise FileNotFoundError("source file extension map %s not found" % commentfile)
+    with open(commentfile) as comments:
+        EXTENSIONS = tuple(COMMENTS := yaml.safe_load(comments))
+    print("extracting snippets in source files", 
+        " ".join(ex for ex in COMMENTS if COMMENTS[ex]), "\n")
+
+    # initialize snipper instance and our counters
+    snipper = Snipper(snippetdir)
+    seen = processed = 0
+
+    # main loop: for each file named on stdin, check to see if we should process it, and do so
+    try:
+
+        for path in sys.stdin:
+            path = path.strip().replace("\\", "/")  # standardize on Linux-style paths
+            if not path:                            # skip blank lines in input
+                continue
+            if not path.startswith("./") and not path.startswith("/") and path[2:3] != ":":
+                path = "./" + path
+            if "/." in path:                        # skip hidden file or directory
+                continue
+            seen += 1                               # count files seen (not hidden)
+            if path.endswith(EXTENSIONS):           # process it if it's a source file
+                snipper(path)
+                processed += 1                      # count files processed
+
+        # print summary of what we processed and exit (successfully)
+        print("\n====", snipper.count, "snippet(s) extracted from", processed, 
+            "source file(s) processed of", seen, "candidate(s)\n")
+        sys.exit(0) 
+
+    except Exception as ex:
+
+        # get all line numbers in traceback
+        tb = ex.__traceback__
+        lines = []
+        while lines.append(str(tb.tb_lineno)) or tb.tb_next:    
+            tb = tb.tb_next
+        # print short error message and exit (unsuccessfully)
+        print(f"@{'.'.join(reversed(lines))} {type(ex).__name__}: {ex}", file=sys.stderr)
+        sys.exit(1)
