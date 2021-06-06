b19b33972fe446aec588871b8bf90edccde5fa87
Katreena Mullican <69986091+mullicak@users.noreply.github.com>
2021-06-06 14:51:01 -0500

    Update rekognition_lambda_s3_trigger_stack.py
    Add specific suffixes for images types supported by rekognition

--- python/rekognition-lambda-s3-trigger/rekognition_lambda_s3_trigger/rekognition_lambda_s3_trigger_stack.py
+++ python/rekognition-lambda-s3-trigger/rekognition_lambda_s3_trigger/rekognition_lambda_s3_trigger_stack.py
@@ -58,7 +58,9 @@ class RekognitionLambdaS3TriggerStack(core.Stack):
         # create trigger for Lambda function
         notification = s3_notifications.LambdaDestination(lambda_function)
         notification.bind(self, bucket)
-        bucket.add_object_created_notification(notification)
+        bucket.add_object_created_notification(notification, s3.NotificationKeyFilter(suffix='.jpg'))
+        bucket.add_object_created_notification(notification, s3.NotificationKeyFilter(suffix='.jpeg'))
+        bucket.add_object_created_notification(notification, s3.NotificationKeyFilter(suffix='.png'))
 
         # grant permissions for lambda to read/write to DynamoDB table and bucket
         table.grant_read_write_data(lambda_function)
