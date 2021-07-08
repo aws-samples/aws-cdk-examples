10239f20e8254b54f61ad249558fe52e4a99666e
Ryan Parker <ryan.parker3@outlook.com>
2021-07-08 18:03:43 -0500

    fix: removed unnecessary files, improved README, removed ts-node dep and other unnecessary deps

--- typescript/ec2-instance/README.md
+++ typescript/ec2-instance/README.md
@@ -1,30 +1,65 @@
 # EC2 Instance Creation with CDK
 
 This example will create:
-- a new VPC
-- two public subnets
-- a security group
-- an instance in one of those subnets
 
-The config.sh file in /src can be updated and will run on first boot as the user data.
+- A new VPC
+- Two public subnets
+- A security group
+- An EC2 instance in one of the subnets
+
+The `/src/config.sh` file is used as [user-data](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html) for the EC2 instance. Update this with any commands you'd like to be executed when the EC2 instance first boots.
+
+[_learn more about user-data_](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html)
 
 ## To Deploy
 
-- Ensure aws-cdk is installed and bootstrapped
-- `npm run build`
-- `cdk deploy`
+Ensure aws-cdk is installed and [bootstrapped](https://docs.aws.amazon.com/cdk/latest/guide/bootstrapping.html).
+
+```bash
+$ npm install -g aws-cdk
+$ cdk bootstrap
+```
+
+Then build and deploy.
+
+```bash
+$ npm run build
+$ cdk deploy
+```
 
 ## Output
 
-Ec2CdkStack.DownloadKeyCommand : The command needed to download the private key that was created
-Ec2CdkStack.IPAddress : Public IP Address of Instance
-Ec2CdkStack.KeyName : Key Name that was created
-Ec2CdkStack.sshcommand : The command used to connect to the instance
+- `Ec2CdkStack.DownloadKeyCommand`: The command needed to download the private key that was created.
+- `Ec2CdkStack.IPAddress`: Public IP Address of Instance.
+- `Ec2CdkStack.KeyName`: Key Name that was created.
+- `Ec2CdkStack.sshcommand`: The command used to connect to the instance.
 
 ## Keys and Access
 
-A Key Pair is created as part of this CDK.  The public key will be installed as an authorized key in the instance.  To connect to the instance, you will need to download the private key from aws secretsmanager.  The aws-cli command to do this is included as part of the Output.  This key will be downloaded as `cdk-key.pem` and permissions changed.  The ssh command to connect is also included and will use this key.  
+A Key Pair is created as part of this project. The public key will be installed as an authorized key in the EC2 instance.
+
+To connect to the instance:
+
+1. Download the private key from aws secretsmanager:
+
+    ```bash
+    # This will downloaded the key as `cdk-key.pem` and grant permissions.
+    $ aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem
+    ```
+
+2. SSH to the instance using the command provided from the stack's output `Ec2CdkStack.sshcommand`.
+
+    For example:
+
+    ```bash
+    $ ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@1.111.11.111
+    ```
+
+    _Find the command for your specific instance in the stack's output._
 
 ## To Destroy
 
-`cdk destroy` will destroy all elements created.
+```bash
+# Destroy all project resources.
+$ cdk destroy
+```
--- typescript/ec2-instance/lib/ec2-cdk-stack.ts
+++ typescript/ec2-instance/lib/ec2-cdk-stack.ts
@@ -1,35 +1,36 @@
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam'
+import * as path from 'path';
 import { KeyPair } from 'cdk-ec2-key-pair';
 import { Asset } from '@aws-cdk/aws-s3-assets';
-import * as path from 'path';
-import * as iam from '@aws-cdk/aws-iam'
-import { CfnVPCEndpointServicePermissions } from "@aws-cdk/aws-ec2";
+
 export class Ec2CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
     // Create a Key Pair to be used with this EC2 Instance
     const key = new KeyPair(this, 'KeyPair', {
       name: 'cdk-keypair',
       description: 'Key Pair created with CDK Deployment',
     });
     key.grantReadOnPublicKey
-    
+
     // Create new VPC with 2 Subnets
     const vpc = new ec2.Vpc(this, 'VPC', {
       natGateways: 0,
-      subnetConfiguration: [ {
+      subnetConfiguration: [{
         cidrMask: 24,
         name: "asterisk",
         subnetType: ec2.SubnetType.PUBLIC
-    }]});
+      }]
+    });
 
     // Allow SSH (TCP Port 22) access from anywhere
     const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
       vpc,
       description: 'Allow SSH (TCP port 22) in',
-      allowAllOutbound: true 
+      allowAllOutbound: true
     });
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
 
@@ -40,9 +41,10 @@ export class Ec2CdkStack extends cdk.Stack {
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
 
     // Use Latest Amazon Linux Image - CPU Type ARM64
-    const ami = new ec2.AmazonLinuxImage({ 
+    const ami = new ec2.AmazonLinuxImage({
       generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      cpuType: ec2.AmazonLinuxCpuType.ARM_64});
+      cpuType: ec2.AmazonLinuxCpuType.ARM_64
+    });
 
     // Create the instance using the Security Group, AMI, and KeyPair defined in the VPC created
     const ec2Instance = new ec2.Instance(this, 'Instance', {
@@ -55,17 +57,17 @@ export class Ec2CdkStack extends cdk.Stack {
     });
 
     // Create an asset that will be used as part of User Data to run on first load
-    const asset = new Asset(this, 'Asset', {path: path.join(__dirname, '../src/config.sh')});
+    const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../src/config.sh') });
     const localPath = ec2Instance.userData.addS3DownloadCommand({
-      bucket:asset.bucket,
-      bucketKey:asset.s3ObjectKey,
+      bucket: asset.bucket,
+      bucketKey: asset.s3ObjectKey,
     });
 
     ec2Instance.userData.addExecuteFileCommand({
-      filePath:localPath,
+      filePath: localPath,
       arguments: '--verbose -y'
     });
-    asset.grantRead( ec2Instance.role );
+    asset.grantRead(ec2Instance.role);
 
     // Create outputs for connecting
     new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
