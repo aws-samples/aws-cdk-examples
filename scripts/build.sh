a16261c672f8d9b4944a1b7714388f9c19937318
Ryan Parker <ryan.parker3@outlook.com>
2021-07-08 16:04:08 -0500

    fix: `build-npm.sh` -> `build-typescript.sh`

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 
-$scriptdir/build-npm.sh
+$scriptdir/build-csharp.sh
 $scriptdir/build-java.sh
 $scriptdir/build-python.sh
-$scriptdir/build-csharp.sh
+$scriptdir/build-typescript.sh
