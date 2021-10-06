b77c84e11ff4e97d6d0a21b05a9c8b24115e9acb
kaizen3031593 <zen.conroy@gmail.com>
2021-10-06 10:25:10 -0400

    initial csharp app

---
+++ csharp/lambda-cron/src/LambdaCron/LambdaCronStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace LambdaCron
+{
+    public class LambdaCronStack : Stack
+    {
+        internal LambdaCronStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
