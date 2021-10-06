9bf0b9df2a05e49a6908f2f35e2ff3086a84d11d
kaizen3031593 <zen.conroy@gmail.com>
2021-10-06 11:01:26 -0400

    add lambda-cron example

--- csharp/lambda-cron/src/LambdaCron/LambdaCronStack.cs
+++ csharp/lambda-cron/src/LambdaCron/LambdaCronStack.cs
@@ -1,12 +1,28 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Events.Targets;
+using Amazon.CDK.AWS.Events;
 
 namespace LambdaCron
 {
     public class LambdaCronStack : Stack
     {
-        internal LambdaCronStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        public LambdaCronStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var lambdaFn = new Function(this, "Singleton", new FunctionProps {
+                Runtime = Runtime.PYTHON_3_6,
+                Code = Code.FromAsset("src/LambdaCron/resources"),
+                Handler = "lambda-handler.main",
+                Timeout = Duration.Seconds(300),
+            });
+
+            // Run 6:00 PM UTC every Monday through Friday
+            // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
+            var rule = new Rule(this, "Rule", new RuleProps {
+                Schedule = Schedule.Expression("cron(0 18 ? * MON-FRI *)"),
+            });
+
+            rule.AddTarget(new LambdaFunction(lambdaFn));
         }
     }
 }
