4f93c8dd7d502757e771d375bce949556b2bc0c3
Justin Jaramillo <jajara@amazon.com>
2021-10-17 22:10:55 -0700

    Rename lambdas

--- typescript/api-eventbridge-lambda/lambda/eventConsumerLambda.ts
+++
@@ -1,8 +0,0 @@
-exports.handler = async function(event) {
-    console.log("Request:", JSON.stringify(event, undefined, 2));
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `testing...`
-    };
-};
