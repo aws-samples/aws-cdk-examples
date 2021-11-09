20051a5a7058f65a798069f6dfabba81ed75ac48
Ryan Parker <ryan.parker3@outlook.com>
2021-11-09 12:47:03 -0800

    Added `vpc_subnets` prop to RDS DB instance

--- python/new-vpc-alb-asg-mysql/cdk_vpc_ec2/cdk_rds_stack.py
+++ python/new-vpc-alb-asg-mysql/cdk_vpc_ec2/cdk_rds_stack.py
@@ -17,7 +17,7 @@ class CdkRdsStack(core.Stack):
         #                                         )
         #                                         instance_props=rds.InstanceProps(
         #                                             vpc=vpc,
-        #                                             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.ISOLATED),
+        #                                             vpc_subnets=ec2.SubnetSelection(subnet_group_name="DB"),
         #                                             instance_type=ec2.InstanceType(instance_type_identifier="t2.small")
         #                                         ),
         #                                         instances=2,
@@ -37,6 +37,7 @@ class CdkRdsStack(core.Stack):
                                              instance_type=ec2.InstanceType.of(
                                                  ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
                                              vpc=vpc,
+                                             vpc_subnets=ec2.SubnetSelection(subnet_group_name="DB"),
                                              multi_az=True,
                                              allocated_storage=100,
                                              storage_type=rds.StorageType.GP2,
