1dcf893b1850af518075a24b677539fbbf71a475
Nick Lynch <nlynch@amazon.com>
2021-12-01 15:08:17 +0000

    feat: upgrade Typescript examples to CDK v2 (#575)
    * feat: upgrade Typescript examples to CDK v2
    
    This PR upgrades all of the Typescript examples to use the latest
    release-candidate version of the CDKv2. The other examples (e.g., C#, Python)
    were already upgraded in #573.
    
    As part of the upgrade, all usages of deprecated CDK APIs have been updated to
    the newer, non-deprecated counterparts.
    
    * chore: fix break in build-typescript.sh

--- typescript/lambda-manage-s3-event-notification/lib/shared-resources-stack.ts
+++ typescript/lambda-manage-s3-event-notification/lib/shared-resources-stack.ts
@@ -1,15 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as iam from '@aws-cdk/aws-iam';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
 import * as path from 'path';
 
 export class SharedStack extends cdk.Stack {
   public readonly bucketName: string;
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'SampleBucket', {
@@ -25,7 +26,7 @@ export class SharedStack extends cdk.Stack {
       reservedConcurrentExecutions: 1,
       timeout: cdk.Duration.seconds(300)
     });
-    
+
     fn.addToRolePolicy(
       new iam.PolicyStatement({
         actions: ['s3:GetBucketNotification', 's3:PutBucketNotification'],
