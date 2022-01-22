196a735760f76ac6aa14a4a8018cd170b64622b6
Rajesh Sitaraman <rajeshrd@amazon.com>
2022-01-21 17:59:45 -0800

    Add detected labels to database

--- typescript/rekognition-lambda-s3-trigger/lambda/rekfunction.ts
+++ typescript/rekognition-lambda-s3-trigger/lambda/rekfunction.ts
@@ -2,13 +2,14 @@ import * as AWS from 'aws-sdk';
 
 const client = new AWS.Rekognition();
 export const handler = async (event: any = {}): Promise<any> => {
-    console.log(event.Records[0].s3.object.key);
+    const key = event.Records[0].s3.object.key
+    console.log(key);
 
     const params: AWS.Rekognition.DetectLabelsRequest = {
         Image: {
             S3Object: {
                 Bucket: process.env.BUCKET_NAME,
-                Name: event.Records[0].s3.object.key
+                Name: key
             },
         },
         MaxLabels: 10,
@@ -17,9 +18,27 @@ export const handler = async (event: any = {}): Promise<any> => {
 
     const response = await client.detectLabels(params).promise();
     const labels = response.Labels || [];
-    console.log(labels);
+    console.log(labels.map(i => i.Name).toString());
 
     // Write to DDB
+    const tableName = process.env.TABLE_NAME || "";
+    const dynamodb = new AWS.DynamoDB();
 
+    const dynamodbParams = {
+        TableName: tableName,
+        Item: {
+            image_name: {'S': key},
+            labels: {'S': labels.map(i => i.Name).toString()}
+        },
+        ConditionExpression: 'attribute_not_exists(image_name)'
+    };
+
+    try {
+        await dynamodb.putItem(dynamodbParams).promise();
+    }
+    catch(err) {
+        console.log(err);
+    }
+    
     return;
 };
