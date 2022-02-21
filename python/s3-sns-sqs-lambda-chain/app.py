df1f3689793a0ac53ecadecff2d630a85611ecc8
Aniket Braganza <aabragan@users.noreply.github.com>
2022-02-21 07:29:20 -0600

    feat(python): removing unused import and removing extra spacing
    Co-authored-by: Ryan Parker <ryan.parker3@outlook.com>

--- python/s3-sns-sqs-lambda-chain/app.py
+++ python/s3-sns-sqs-lambda-chain/app.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
-
 from s3_sns_sqs_lambda_chain.s3_sns_sqs_lambda_chain_stack import S3SnsSqsLambdaChainStack
-
 
 app = cdk.App()
 S3SnsSqsLambdaChainStack(
