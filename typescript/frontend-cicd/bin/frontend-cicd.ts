64607188567cc276468d5848cf71191a7359c6d6
jinspark-lab <jinsangp@gmail.com>
2022-05-15 20:00:52 +0900

    Fix hard-coded-acc_id

--- typescript/frontend-cicd/bin/frontend-cicd.ts
+++ typescript/frontend-cicd/bin/frontend-cicd.ts
@@ -15,7 +15,7 @@ new FrontendCicdStack(app, 'FrontendCicdStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '486403792456', region: 'us-east-1' },
+  env: { account: '<YOUR_ACCOUNT_ID>', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
