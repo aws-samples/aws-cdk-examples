433087f05792bb199dcd5141a8c10d4c23d515d6
Enrico Liguori <elliguor@amazon.com>
2022-12-14 17:22:40 +0100

    changed AMI to use Amazon Linux 2 and mysql to the latest version. Improved Readme

--- python/vpc-ec2-local-zones/README.md
+++ python/vpc-ec2-local-zones/README.md
@@ -1,26 +1,25 @@
-
 # Local Zone: Getting started with CDK
 
 This is a project that demonstrates how to deploy resources in a AWS Local Zone.
 
-## Overview 
+## Overview
 
 ![alt text](./architecture.png "AWS Local Zone with CDK")
 
-The generated stack creates a new Amazon VPC with a public and a private subnet associated with the specified Local Zone. 
-To allow resources in the private subnet to initiate traffic towards the Internet, it deploys an EC2 instance that acts as a NAT instance. The reason why a self managed NAT instance is used is because currently NAT Gateway is not supported in Local Zone. 
+The generated stack creates a new Amazon VPC with a public and a private subnet associated with the specified Local Zone.
+To allow resources in the private subnet to initiate traffic towards the Internet, it deploys an EC2 instance that acts as a NAT instance. The reason why a self managed NAT instance is used is because currently NAT Gateway is not supported in Local Zone.
 
 The NAT instance is configured following the best practices described in [the public documentation](https://docs.aws.amazon.com/vpc/latest/userguide/VPC_NAT_Instance.html#basics). The route table of the private instance is automatically configured to have a default route pointing to the NAT instance in the public subnet.
 
 In addition, to demonstrate how to use other services in the Local Zone, the script will deploy:
 
-1. an EC2 in private subnet with WordPress installed through the User Data script. This EC2 is part of an AutoScaling Group with desired and maximum capacity at 1. The role of the AS Group will be to maintain the EC2 with Wordpress always healthy; 
+1. an EC2 in private subnet with WordPress installed through the User Data script. This EC2 is part of an AutoScaling Group with desired and maximum capacity at 1. The role of the AS Group will be to maintain the EC2 with Wordpress always healthy;
 2. a MySQL database installed in a EC2 through User Data script in the private subnet;
 3. an Application Load Balancer deployed in the public subnet that can be used as a single point of access to the WordPress application.
 
-## Local Zone 
+## Local Zone
 
-For this sample it is used Los Angeles Local Zone. 
+For this sample it is used Atlanta Local Zone.
 To use a different Local Zone, please change *LZ_NAME* variable in *vpc_ec2_local_zones/vpc_ec2_local_zones_stack.py*
 
 ```
@@ -30,19 +29,53 @@ LZ_NAME="us-east-1-atl-1a"
 VPC_CIDR="172.31.100.0/24"
 # Subnet size of the subnets in the Local Zone
 SUBNET_SIZE=26
+
+[...]
+
+vpc = ec2.Vpc(
+            self, 
+            "Vpc",
+            cidr=”172.31.100.0/24”,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name = 'Public-Subent',
+                    subnet_type = ec2.SubnetType.PUBLIC,
+                    cidr_mask = 26,
+                ),
+                ec2.SubnetConfiguration(
+                    name = 'Private-Subent',
+                    subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidr_mask = 26,
+                ),
+            ]       
+        )
+
+[...]
 ```
+Please note that in the subnet definitions above there is no specification of the Availability Zone or AWS Local Zone that we want to associate with them. We can define this setting at VPC level, overwriting the availability_zones method as shown below: 
+
+```
+@property
+def availability_zones(self):
+   return [“us-east-1-las-1a”] 
+```
+
+As an alternative, you can pass a Local Zone name as value of availability_zones parameter in each [Subnet](https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_ec2/Subnet.html) definition.
+
 
 ## Instance types
 
-In this sample the Instance Type used for the EC2 instances is **T3 Medium**. 
+In this sample the Instance Type used for the EC2 instances is **T3 Medium**.
 You can check the supported instance types in a specific Local Zone with [DescribeInstanceTypeOfferings](https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeInstanceTypeOfferings.html) API (please note that the Local Zone must be enabled to use the API).
 
-CLI command: 
+CLI command:
+
 ```
 aws ec2 describe-instance-type-offerings --location-type "availability-zone" --filters Name=location,Values=<LZ Name> --region <Region>
 ```
 
-or using Boto3: 
+or using Boto3:
+
 ```
 ec2 = boto3.client('ec2', config=Config(region_name=<region>))
 response = ec2.describe_instance_type_offerings(
@@ -58,7 +91,7 @@ response = ec2.describe_instance_type_offerings(
 
 ## Application Load Balancer
 
-Please be aware that currently Application Load Balancer is not supported in the Local Zones outside of US. If you plan to use a Local Zone where ALB is not available, you will need to replace it with a self managed EC2 instance. 
+Please be aware that currently Application Load Balancer is not supported in all the Local Zones. If you plan to use a Local Zone where ALB is not available, you will need to replace it deploying a load balancer in a self managed EC2 instance or use or use a load balancer available in [AWS Marketplace](https://aws.amazon.com/marketplace)
 
 ## Security Groups
 
@@ -67,7 +100,7 @@ The Security Groups rules are tailored to allow only the traffic needed to acces
 1. The SG attached to the ALB allow only HTTP traffic on port 80 in Ingress and TCP traffic on port 8080 (the port used by WordPress to accept requests) in Egress;
 2. The SGs attached to the EC2 with WordPress allows only TCP traffic on the service port (i.e. 8080) in Ingress from the ALB;
 3. The SGs attached to the EC2 with MySQL database allows only TCP traffic on the service port (i.e. 3306) in Ingress from the WordPress instance;
-4. The SG attached to the NAT instance allows only HTTP and HTTPS traffic from the VPC CIDR in Ingress and same type of traffic to everywhere in Egress. 
+4. The SG attached to the NAT instance allows only HTTP and HTTPS traffic from the VPC CIDR in Ingress and same type of traffic to everywhere in Egress.
 
 ## Usage
 
@@ -83,14 +116,14 @@ you can create the virtualenv manually.
 To manually create a virtualenv on MacOS and Linux:
 
 ```
-$ python3 -m venv .venv
+python3 -m venv .venv
 ```
 
 After the init process completes and the virtualenv is created, you can use the following
 step to activate your virtualenv.
 
 ```
-$ source .venv/bin/activate
+source .venv/bin/activate
 ```
 
 If you are a Windows platform, you would activate the virtualenv like this:
@@ -102,13 +135,13 @@ If you are a Windows platform, you would activate the virtualenv like this:
 Once the virtualenv is activated, you can install the required dependencies.
 
 ```
-$ pip install -r requirements.txt
+pip install -r requirements.txt
 ```
 
 At this point you can now synthesize the CloudFormation template for this code.
 
 ```
-$ cdk synth
+cdk synth
 ```
 
 To add additional dependencies, for example other CDK libraries, just add
@@ -117,8 +150,8 @@ command.
 
 ## Useful commands
 
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
+* `cdk ls`          list all stacks in the app
+* `cdk synth`       emits the synthesized CloudFormation template
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk docs`        open CDK documentation
--- python/vpc-ec2-local-zones/vpc_ec2_local_zones/vpc_ec2_local_zones_stack.py
+++ python/vpc-ec2-local-zones/vpc_ec2_local_zones/vpc_ec2_local_zones_stack.py
@@ -1,3 +1,5 @@
+import re
+
 from aws_cdk import (
     Stack,
     CfnOutput,
@@ -6,29 +8,29 @@ from aws_cdk import (
     aws_autoscaling as autoscaling,
 )
 from constructs import Construct
-import re
 
 # Local Zone to be used
-LZ_NAME="us-east-1-atl-1a"
+LZ_NAME = "us-east-1-atl-1a"
 # VPC CIDR that will be used to create a new VPC
-VPC_CIDR="172.31.100.0/24"
+VPC_CIDR = "172.31.100.0/24"
 # Subnet size of the subnets in the Local Zone
-SUBNET_SIZE=26
+SUBNET_SIZE = 26
+
 
 class VpcEc2LocalZonesStack(Stack):
 
-    # overwrite availability_zones method used by CDK to determine the 
+    # overwrite availability_zones method used by CDK to determine the
     # Avaialability Zones or the Local Zone to be used by ec2.VPC construct
     @property
     def availability_zones(self):
        return [LZ_NAME]
 
-    def create_VPC(self): 
+    def create_VPC(self):
         vpc = ec2.Vpc(
-            self, 
+            self,
             "Vpc",
-            cidr=VPC_CIDR,
-            subnet_configuration=[
+            cidr = VPC_CIDR,
+            subnet_configuration = [
                 ec2.SubnetConfiguration(
                     name = 'Public-Subent',
                     subnet_type = ec2.SubnetType.PUBLIC,
@@ -39,15 +41,15 @@ class VpcEc2LocalZonesStack(Stack):
                     subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
                     cidr_mask = SUBNET_SIZE,
                 ),
-            ]       
+            ]
         )
         return vpc
 
     # method to create a NAT Instance in the public subnet
-    # see user_data/nat_instance txt file to see the User Data script 
+    # see user_data/nat_instance txt file to see the User Data script
     # used to configured the NAT rules
     def create_nat_instance(self, vpc):
-        amzn_linux = ec2.MachineImage.latest_amazon_linux()
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2)
         user_data = self.get_user_data("nat_instance")
         nat = ec2.Instance(self, "NATInstanceInLZ",
                  vpc=vpc,
@@ -55,16 +57,16 @@ class VpcEc2LocalZonesStack(Stack):
                  instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
                  machine_image=amzn_linux,
                  user_data=ec2.UserData.custom(user_data),
-                 vpc_subnets=ec2.SubnetSelection(availability_zones=[LZ_NAME],subnet_type=ec2.SubnetType.PUBLIC),
+                 vpc_subnets=ec2.SubnetSelection(availability_zones=[LZ_NAME], subnet_type=ec2.SubnetType.PUBLIC),
                  source_dest_check=False
                 )
         return nat
 
     # method to add a default route in the private subnet pointing to the NAT instance
     def add_route_to_nat(self, vpc, nat_instance):
-        #select the private subnet created before
+        # select the private subnet created before
         priv_subnet = ec2.SubnetFilter.availability_zones([LZ_NAME]).select_subnets(vpc.isolated_subnets)
-        #add the default route pointing to the nat instance
+        # add the default route pointing to the nat instance
         priv_subnet[0].add_route("DefRouteToNAT",
             router_id=nat_instance.instance_id,
             router_type=ec2.RouterType.INSTANCE,
@@ -84,12 +86,12 @@ class VpcEc2LocalZonesStack(Stack):
             peer=ec2.Peer.ipv4(VPC_CIDR),
             connection=ec2.Port.tcp(80),
             description="HTTP ingress",
-        ) 
+        )
         sg.add_ingress_rule(
             peer=ec2.Peer.ipv4(VPC_CIDR),
             connection=ec2.Port.tcp(443),
             description="HTTPS ingress",
-        ) 
+        )
         sg.add_egress_rule(
             peer=ec2.Peer.ipv4("0.0.0.0/0"),
             connection=ec2.Port.tcp(80),
@@ -99,30 +101,31 @@ class VpcEc2LocalZonesStack(Stack):
             peer=ec2.Peer.ipv4("0.0.0.0/0"),
             connection=ec2.Port.tcp(443),
             description="HTTPS egress",
-        )   
+        )
         return sg
 
-    def create_ALB_in_lz(self,vpc,wp_as):
-        alb = elbv2.ApplicationLoadBalancer(self, "ALB_in_LZ", 
-                 vpc=vpc,
-                 internet_facing=True,
-                 vpc_subnets=ec2.SubnetSelection(availability_zones=[LZ_NAME],subnet_type=ec2.SubnetType.PUBLIC),
-                )
-        http_listener = alb.add_listener("ListenerHTTP",port=80)
-        tg = http_listener.add_targets("AppFleet",port=8080,targets=[wp_as])
-        #WP will reply to ALB health-checks with a 301 HTTP code. we need to asdjust tg configuration accordingly
+    def create_ALB_in_lz(self, vpc, wp_as):
+        alb = elbv2.ApplicationLoadBalancer(self,
+        "ALB_in_LZ",
+        vpc=vpc,
+        internet_facing=True,
+        vpc_subnets=ec2.SubnetSelection(availability_zones=[LZ_NAME],subnet_type=ec2.SubnetType.PUBLIC),
+        )
+        http_listener = alb.add_listener("ListenerHTTP", port=80)
+        tg = http_listener.add_targets("AppFleet", port=8080,targets=[wp_as])
+        # WP will reply to ALB health-checks with a 301 HTTP code. we need to asdjust tg configuration accordingly
         tg.configure_health_check(
             healthy_http_codes = "200,301"
         )
-        #allow traffic from ALB on target instance
+        # allow traffic from ALB on target instance
         wp_as.connections.allow_from(alb, ec2.Port.tcp(8080), "ALB access on target instance")
         return alb
 
 
     # method to create a new EC2 instance with MySQL installed through User Data script
-    # see user_data/db_mysql txt file to see the User Data script 
+    # see user_data/db_mysql txt file to see the User Data script
     def create_db_mysql(self, vpc):
-        amzn_linux = ec2.MachineImage.latest_amazon_linux()
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2)
         user_data = self.get_user_data("db_mysql")
         db = ec2.Instance(self, "DBInLZ",
                  vpc=vpc,
@@ -134,22 +137,24 @@ class VpcEc2LocalZonesStack(Stack):
         return db
 
     # method to create a new AutoScaling group for managing WordPress instances.
-    # see user_data/wp_webserver txt file to see the User Data script 
+    # see user_data/wp_webserver txt file to see the User Data script
     def create_wp_webserver(self, vpc, db):
-        amzn_linux = ec2.MachineImage.latest_amazon_linux()
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2)
         user_data = self.get_user_data("wp_webserver")
         # we need to modify the user data script to provide the dns name of the
-        # mysql database installed in the private subnet. 
+        # mysql database installed in the private subnet.
         user_data = re.sub('dbhost', db.instance_private_dns_name, user_data)
 
-        wp_as = autoscaling.AutoScalingGroup(self, "WordPressAS", 
+        wp_as = autoscaling.AutoScalingGroup(self,
+                "WordPressAS",
                 vpc=vpc,
                 instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
                 machine_image=amzn_linux,
                 user_data=ec2.UserData.custom(user_data),
-                vpc_subnets=ec2.SubnetSelection(availability_zones=[LZ_NAME],subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
-                )
-        #allow connections from wp_instance to db
+                vpc_subnets=ec2.SubnetSelection(availability_zones=[LZ_NAME],
+                                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
+        )
+        # allow connections from wp_instance to db
         db.connections.allow_from(wp_as, ec2.Port.tcp(3306), "WP access on db port")
         return wp_as
 
@@ -161,17 +166,17 @@ class VpcEc2LocalZonesStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        #create new VPC in the Local Zone
+        # create new VPC in the Local Zone
         vpc = self.create_VPC()
 
-        #create NAT instance
+        # create NAT instance
         nat_instance = self.create_nat_instance(vpc)
         self.add_route_to_nat(vpc, nat_instance)
 
-        #create db mysql
+        # create db mysql
         db = self.create_db_mysql(vpc)
         # create an AutoScaling groups to manage web server with WordPress
         wp_as = self.create_wp_webserver(vpc, db)
-        #create ALB
+        # create ALB
         alb = self.create_ALB_in_lz(vpc, wp_as)
         CfnOutput(self, "ALB DNS name: ", value=alb.load_balancer_dns_name)
