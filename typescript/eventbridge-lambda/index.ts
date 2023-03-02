299ffb7602277ed566f97394699b0a1d40c6b5f5
Michael Webb <17991861+BrandonMichaelWebb@users.noreply.github.com>
2023-03-02 06:24:13 -0700

    issue-615
    (typescript): An example using EventBridge rule to schedule Lambda function

---
+++ typescript/eventbridge-lambda/index.ts
@@ -0,0 +1,30 @@
+import events = require('aws-cdk-lib/aws-events');
+import targets = require('aws-cdk-lib/aws-events-targets');
+import lambda = require('aws-cdk-lib/aws-lambda');
+import cdk = require('aws-cdk-lib');
+
+import fs = require('fs');
+
+export class EventBridgeLambdaStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id);
+
+    const lambdaFn = new lambda.Function(this, 'Singleton', {
+      code: new lambda.InlineCode(fs.readFileSync('lambda-handler.py', { encoding: 'utf-8' })),
+      handler: 'index.main',
+      timeout: cdk.Duration.seconds(300),
+      runtime: lambda.Runtime.PYTHON_3_9,
+    });
+
+    // Run 6:00 PM UTC every Monday through Friday
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.expression('cron(0 18 ? * MON-FRI *)')
+    });
+
+    rule.addTarget(new targets.LambdaFunction(lambdaFn));
+  }
+}
+
+const app = new cdk.App();
+new EventBridgeLambdaStack(app, 'EventBridgeLambdaStack');
+app.synth();
