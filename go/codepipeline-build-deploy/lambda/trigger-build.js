7382f953222d53ea3f12d3f37d0d3896f43d870c
Yaseen Younus <yaseenyx@amazon.com>
2023-04-02 18:15:30 -0500

    Code clean up + adding dependency for faster deploy

--- go/codepipeline-build-deploy/lambda/trigger-build.js
+++ go/codepipeline-build-deploy/lambda/trigger-build.js
@@ -4,21 +4,18 @@ const {
 } = require("@aws-sdk/client-codebuild");
 
 exports.handler = async (event) => {
-  const REGION = "us-east-1";
-
-  const codebuild = new CodeBuildClient({ region: REGION });
-
+  const region = process.env.REGION;
   const buildProjectName = process.env.CODEBUILD_PROJECT_NAME;
-  console.log(buildProjectName);
 
+  const codebuild = new CodeBuildClient({ region: region });
   const buildCommand = new StartBuildCommand({ projectName: buildProjectName });
+
   console.log("Triggering CodeBuild Project...");
   const buildResponse = await codebuild.send(buildCommand);
   console.log(buildResponse);
 
-  const response = {
+  return {
     statusCode: 200,
     body: "CodeBuild Project building...",
   };
-  return response;
 };
