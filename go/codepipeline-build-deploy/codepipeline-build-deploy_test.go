87e00dfb41275332eb925b986c1673861d22695a
Yaseen Younus <yaseenyx@amazon.com>
2023-04-03 12:15:25 -0500

    Updates README + adds go tests

--- go/codepipeline-build-deploy/codepipeline-build-deploy_test.go
+++ go/codepipeline-build-deploy/codepipeline-build-deploy_test.go
@@ -1,26 +1,54 @@
 package main
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/aws/aws-cdk-go/awscdk/v2"
-// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-// 	"github.com/aws/jsii-runtime-go"
-// )
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	"github.com/aws/jsii-runtime-go"
+)
 
-// example tests. To run these tests, uncomment this file along with the
-// example resource in codepipeline-docker-build_test.go
-// func TestCodepipelineDockerBuildStack(t *testing.T) {
-// 	// GIVEN
-// 	app := awscdk.NewApp(nil)
+func TestCodePipelineBuildDeployStack(t *testing.T) {
+	// GIVEN
+	app := awscdk.NewApp(nil)
 
-// 	// WHEN
-// 	stack := NewCodepipelineDockerBuildStack(app, "MyStack", nil)
+	// WHEN
+	stack := NewCodePipelineBuildDeployStack(app, "MyStack", nil)
 
-// 	// THEN
-// 	template := assertions.Template_FromStack(stack)
+	// THEN
+	template := assertions.Template_FromStack(stack, nil)
 
-// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-// 		"VisibilityTimeout": 300,
-// 	})
-// }
+	// Checks if the ALB listens on port 80
+	template.HasResourceProperties(jsii.String("AWS::ElasticLoadBalancingV2::Listener"), map[string]any{
+		"Port": 80,
+	})
+	
+	// Checks if the ECS Deployment Controller is set to AWS CodeDeploy
+	template.HasResourceProperties(jsii.String("AWS::ECS::Service"), map[string]any{
+		"DeploymentController": map[string]any{
+			"Type": "CODE_DEPLOY",
+		},
+	})
+
+	// Checks if the ALB Security Group allows all traffic on port 80
+	template.HasResourceProperties(jsii.String("AWS::EC2::SecurityGroup"), map[string]any{
+		"SecurityGroupIngress": []any{
+			map[string]any{
+				"CidrIp": "0.0.0.0/0",
+				"FromPort": 80,
+				"IpProtocol": "tcp",
+				"ToPort": 80,
+			},
+		},
+	})
+
+	// Checks if public access to the S3 Bucket is disabled
+	template.HasResourceProperties(jsii.String("AWS::S3::Bucket"), map[string]any{
+		"PublicAccessBlockConfiguration": map[string]any{
+			"BlockPublicAcls":       true,
+			"BlockPublicPolicy":     true,
+			"IgnorePublicAcls":      true,
+			"RestrictPublicBuckets": true,
+		},
+	})
+}
