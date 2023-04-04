14ce02edda72097c9ca390b65235b748dd28debc
Yaseen Younus <yaseenyx@amazon.com>
2023-04-04 17:07:33 -0500

    Initial commit of CDK Typescript CodePipeline code

---
+++ typescript/codepipeline-build-deploy/test/codepipeline-build-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodepipelineBuildDeploy from '../lib/codepipeline-build-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/codepipeline-build-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodepipelineBuildDeploy.CodepipelineBuildDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
