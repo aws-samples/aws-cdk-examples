1ae9273a34bb4f778eb8877a713a9d93e1e8dc1f
Yaseen Younus <yaseenyx@amazon.com>
2023-04-05 18:27:53 -0500

    Adding TypeScript tests

--- typescript/codepipeline-build-deploy/test/codepipeline-build-deploy.test.ts
+++ typescript/codepipeline-build-deploy/test/codepipeline-build-deploy.test.ts
@@ -1,17 +1,50 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CodepipelineBuildDeploy from '../lib/codepipeline-build-deploy-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { CodepipelineBuildDeployStack } from "../lib/codepipeline-build-deploy-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/codepipeline-build-deploy-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CodepipelineBuildDeploy.CodepipelineBuildDeployStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+// Checks if the ECS Deployment Controller is set to AWS CodeDeploy
+test("Deployment Controller Set", () => {
+  const app = new cdk.App();
+  const stack = new CodepipelineBuildDeployStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties("AWS::ECS::Service", {
+    DeploymentController: {
+      Type: "CODE_DEPLOY",
+    },
+  });
+});
+
+// Checks if the ALB Security Group allows all traffic on port 80
+test("Security Group Port 80 Open", () => {
+  const app = new cdk.App();
+  const stack = new CodepipelineBuildDeployStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties("AWS::EC2::SecurityGroup", {
+    SecurityGroupIngress: [
+      {
+        CidrIp: "0.0.0.0/0",
+        FromPort: 80,
+        IpProtocol: "tcp",
+        ToPort: 80,
+      },
+    ],
+  });
+});
+
+// Checks if public access to the S3 Bucket is disabled
+test("S3 Bucket Restricted", () => {
+  const app = new cdk.App();
+  const stack = new CodepipelineBuildDeployStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties("AWS::S3::Bucket", {
+    PublicAccessBlockConfiguration: {
+      BlockPublicAcls: true,
+      BlockPublicPolicy: true,
+      IgnorePublicAcls: true,
+      RestrictPublicBuckets: true,
+    },
+  });
 });
