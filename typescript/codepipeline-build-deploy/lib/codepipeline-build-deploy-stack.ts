9434f01baa54c6b6148e3393341b26ee339f39d2
Yaseen Younus <yaseenyx@amazon.com>
2023-04-05 17:33:47 -0500

    Updating with CDK CodePipeline, ECS and ALB code

--- typescript/codepipeline-build-deploy/lib/codepipeline-build-deploy-stack.ts
+++ typescript/codepipeline-build-deploy/lib/codepipeline-build-deploy-stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from "aws-cdk-lib";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import * as codedeploy from "aws-cdk-lib/aws-codedeploy";
+import * as pipeline from "aws-cdk-lib/aws-codepipeline";
+import * as pipelineactions from "aws-cdk-lib/aws-codepipeline-actions";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as ecs from "aws-cdk-lib/aws-ecs";
-import * as codebuild from "aws-cdk-lib/aws-codebuild";
-import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as elb from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as custom from "aws-cdk-lib/custom-resources";
 import { Construct } from "constructs";
 import * as path from "path";
 
@@ -43,14 +49,14 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
       environment: {
         privileged: true,
         environmentVariables: {
-          REGION: { value: process.env.CDK_DEFAULT_REGION! },
           AWS_ACCOUNT_ID: { value: process.env.CDK_DEFAULT_ACCOUNT! },
+          REGION: { value: process.env.CDK_DEFAULT_REGION! },
           IMAGE_TAG: { value: "latest" },
           IMAGE_REPO_NAME: { value: imageRepo.repositoryName },
           REPOSITORY_URI: { value: imageRepo.repositoryUri },
+          TASK_DEFINITION_ARN: { value: fargateTaskDef.taskDefinitionArn },
           TASK_ROLE_ARN: { value: fargateTaskDef.taskRole.roleArn },
           EXECUTION_ROLE_ARN: { value: fargateTaskDef.executionRole?.roleArn },
-          TASK_DEFINITION_ARN: { value: fargateTaskDef.taskDefinitionArn },
         },
       },
     });
@@ -59,15 +65,203 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
     imageRepo.grantPullPush(buildImage);
 
     // Lambda function that triggers CodeBuild image build project
-    new lambda.Function(this, "BuildLambda", {
+    const triggerCodeBuild = new lambda.Function(this, "BuildLambda", {
       architecture: lambda.Architecture.ARM_64,
       code: lambda.AssetCode.fromAsset(path.join(__dirname, "../lambda"), {}),
       handler: "trigger-build.handler",
       runtime: lambda.Runtime.NODEJS_18_X,
       environment: {
-        REGION: process.env.CDK_DEFAULT_ACCOUNT!,
+        REGION: process.env.CDK_DEFAULT_REGION!,
         CODEBUILD_PROJECT_NAME: buildImage.projectName,
       },
+      // Allows this Lambda function to trigger the buildImage CodeBuild project
+      initialPolicy: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ["codebuild:StartBuild"],
+          resources: [buildImage.projectArn],
+        }),
+      ],
+    });
+
+    // Triggers a Lambda function using AWS SDK
+    const triggerLambda = new custom.AwsCustomResource(
+      this,
+      "BuildLambdaTrigger",
+      {
+        installLatestAwsSdk: true,
+        policy: custom.AwsCustomResourcePolicy.fromStatements([
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["lambda:InvokeFunction"],
+            resources: [triggerCodeBuild.functionArn],
+          }),
+        ]),
+        onCreate: {
+          service: "Lambda",
+          action: "invoke",
+          physicalResourceId: custom.PhysicalResourceId.of("id"),
+          parameters: {
+            FunctionName: triggerCodeBuild.functionName,
+            InvocationType: "Event",
+          },
+        },
+        onUpdate: {
+          service: "Lambda",
+          action: "invoke",
+          parameters: {
+            FunctionName: triggerCodeBuild.functionName,
+            InvocationType: "Event",
+          },
+        },
+      }
+    );
+
+    // Creates VPC for the ECS Cluster
+    const clusterVpc = new ec2.Vpc(this, "ClusterVpc", {
+      ipAddresses: ec2.IpAddresses.cidr("10.50.0.0/16"),
+    });
+
+    // Deploys the cluster VPC after the initial image build triggers
+    clusterVpc.node.addDependency(triggerLambda);
+
+    // Creates a new blue Target Group that routes traffic from the public Application Load Balancer (ALB) to the
+    // registered targets within the Target Group e.g. (EC2 instances, IP addresses, Lambda functions)
+    // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html
+    const targetGroupBlue = new elb.ApplicationTargetGroup(
+      this,
+      "BlueTargetGroup",
+      {
+        targetGroupName: "alb-blue-tg",
+        targetType: elb.TargetType.IP,
+        port: 80,
+        vpc: clusterVpc,
+      }
+    );
+
+    // Creates a new green Target Group
+    const targetGroupGreen = new elb.ApplicationTargetGroup(
+      this,
+      "GreenTargetGroup",
+      {
+        targetGroupName: "alb-green-tg",
+        targetType: elb.TargetType.IP,
+        port: 80,
+        vpc: clusterVpc,
+      }
+    );
+
+    // Creates a Security Group for the Application Load Balancer (ALB)
+    const albSg = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc: clusterVpc,
+      allowAllOutbound: true,
+    });
+    albSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Allows access on port 80/http",
+      false
+    );
+
+    // Creates a public ALB
+    const publicAlb = new elb.ApplicationLoadBalancer(this, "PublicAlb", {
+      vpc: clusterVpc,
+      internetFacing: true,
+      securityGroup: albSg,
+    });
+
+    // Adds a listener on port 80 to the ALB
+    const albListener = publicAlb.addListener("AlbListener80", {
+      open: false,
+      port: 80,
+      defaultTargetGroups: [targetGroupBlue],
+    });
+
+    // Creates an ECS Fargate service
+    const fargateService = new ecs.FargateService(this, "FargateService", {
+      desiredCount: 1,
+      serviceName: "fargate-frontend-service",
+      taskDefinition: fargateTaskDef,
+      cluster: new ecs.Cluster(this, "EcsCluster", {
+        enableFargateCapacityProviders: true,
+        vpc: clusterVpc,
+      }),
+      // Sets CodeDeploy as the deployment controller
+      deploymentController: {
+        type: ecs.DeploymentControllerType.CODE_DEPLOY,
+      },
+    });
+
+    // Adds the ECS Fargate service to the ALB target group
+    fargateService.attachToApplicationTargetGroup(targetGroupBlue);
+
+    // Creates new pipeline artifacts
+    const sourceArtifact = new pipeline.Artifact("SourceArtifact");
+    const buildArtifact = new pipeline.Artifact("BuildArtifact");
+
+    // Creates the source stage for CodePipeline
+    const sourceStage = {
+      stageName: "Source",
+      actions: [
+        new pipelineactions.CodeCommitSourceAction({
+          actionName: "CodeCommit",
+          branch: "main",
+          output: sourceArtifact,
+          repository: codeRepo,
+        }),
+      ],
+    };
+
+    // Creates the build stage for CodePipeline
+    const buildStage = {
+      stageName: "Build",
+      actions: [
+        new pipelineactions.CodeBuildAction({
+          actionName: "DockerBuildPush",
+          input: new pipeline.Artifact("SourceArtifact"),
+          project: buildImage,
+          outputs: [buildArtifact],
+        }),
+      ],
+    };
+
+    // Creates a new CodeDeploy Deployment Group
+    const deploymentGroup = new codedeploy.EcsDeploymentGroup(
+      this,
+      "CodeDeployGroup",
+      {
+        service: fargateService,
+        // Configurations for CodeDeploy Blue/Green deployments
+        blueGreenDeploymentConfig: {
+          listener: albListener,
+          blueTargetGroup: targetGroupBlue,
+          greenTargetGroup: targetGroupGreen,
+        },
+      }
+    );
+
+    // Creates the deploy stage for CodePipeline
+    const deployStage = {
+      stageName: "Deploy",
+      actions: [
+        new pipelineactions.CodeDeployEcsDeployAction({
+          actionName: "EcsFargateDeploy",
+          appSpecTemplateInput: buildArtifact,
+          taskDefinitionTemplateInput: buildArtifact,
+          deploymentGroup: deploymentGroup,
+        }),
+      ],
+    };
+
+    // Creates an AWS CodePipeline with source, build, and deploy stages
+    new pipeline.Pipeline(this, "BuildDeployPipeline", {
+      pipelineName: "ImageBuildDeployPipeline",
+      stages: [sourceStage, buildStage, deployStage],
+    });
+
+    // Outputs the ALB public endpoint
+    new cdk.CfnOutput(this, "PublicAlbEndpoint", {
+      value: "http://" + publicAlb.loadBalancerDnsName,
     });
   }
 }
