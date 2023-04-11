4230a3c31f19306a9ffd299c050373c1a59c76b1
Young-ook Kim <youngook.kim@gmail.com>
2023-04-11 13:11:38 +0900

    fix(python/lambda): remove package install command from deploy script

--- python/s3-efs-sync-lambda/deploy.sh
+++ python/s3-efs-sync-lambda/deploy.sh
@@ -9,5 +9,4 @@ python3 -m venv .env
 source .env/bin/activate
 pip install pip --upgrade
 pip install -r requirements.txt
-pip install urllib3 --target=./func
 cdk deploy --all --require-approval never
