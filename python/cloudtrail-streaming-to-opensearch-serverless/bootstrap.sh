0d249795873e0e288a81ba76b07c08b77aa7027e
Rupesh Tiwari <rupeshti@amazon.com>
2023-04-28 23:12:30 -0400

    docs(cloudtrail-streaming-to-opensearch-serverless): fixing readme file
    readme update

--- python/cloudtrail-streaming-to-opensearch-serverless/bootstrap.sh
+++ python/cloudtrail-streaming-to-opensearch-serverless/bootstrap.sh
@@ -12,7 +12,7 @@ source .env/bin/activate
 # Load dependency for lambda functions,
 .env/bin/python -m pip install --target lambda/build_os_client_and_bulk_ingest_logevents_handler/ -r lambda/build_os_client_and_bulk_ingest_logevents_handler/requirements.txt
 
-# and add boto3 which would be added as layer for metrics, as latest boto3 is needed for serverless collection
+# load boto3
 .env/bin/python -m pip install boto3
 
 # Bootstrap the environment
