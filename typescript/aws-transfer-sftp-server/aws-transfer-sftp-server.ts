e7cd1ae7f2ff863243f301e951103d9e7e785f55
Markus Lindqvist <mlindqv@amazon.com>
2023-06-15 15:55:38 +0300

    feat: Pass in S3 bucket as a parameter

--- typescript/aws-transfer-sftp-server/aws-transfer-sftp-server.ts
+++ typescript/aws-transfer-sftp-server/aws-transfer-sftp-server.ts
@@ -11,23 +11,25 @@ import { Construct } from 'constructs';
 
 /** Properties required to setup the SFTP server. */
 export interface SftpServerStackProps extends StackProps {
-  /** User name for the default user.
+  /** User name for the default SFTP user.
    * @example 'sftp-user'
    */
   userName: string;
-  /** The public key of the user */
+
+  /** The public key of the SFTP user. */
   userPublicKeys: string[];
-  /** Allowed IPs. 
+
+  /** IP addresses that are allowed to connect to the SFTP server.
    * @default All IPV4 is allowed.
    */
   allowedIps?: string[];
+
+  /** The S3 bucket to be configured for the SFTP server. */
+  dataBucket: s3.IBucket;
 }
 
 /** Stack for initializing a fully working SFTP server. */
 export class SftpServerStack extends Stack {
-
-  /** S3 bucket for storing the incoming data. */
-  incomingDataBucket: s3.Bucket;
 
   /** CloudWatch alarm that is triggered if there are too many errors in the logs. */
   errorAlarm: cw.Alarm;
@@ -40,16 +42,8 @@ export class SftpServerStack extends Stack {
       natGateways: 0,
     });
 
-    this.incomingDataBucket = new s3.Bucket(this, 'IncomingDataBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      bucketName: `sftp-server-data-bucket-${this.account}-${this.region}`,
-      encryption: s3.BucketEncryption.KMS_MANAGED,
-      enforceSSL: true,
-      // Do not use for production
-      removalPolicy: RemovalPolicy.DESTROY,
-    });
-
-    // Allow SFTP server to log to CloudWatch
+    // Create the required IAM role which allows the SFTP server
+    // to log to CloudWatch.
     const cloudWatchLoggingRole = new iam.Role(this, 'CloudWatchLoggingRole', {
       assumedBy: new iam.ServicePrincipal('transfer.amazonaws.com'),
       description: 'IAM role used by AWS Transfer for logging',
@@ -69,6 +63,7 @@ export class SftpServerStack extends Stack {
       },
     });
 
+    // Security group for restricting incoming traffic to specific IP addresses
     const sg = new ec2.SecurityGroup(this, 'SftpServerSG', {
       vpc,
       allowAllOutbound: false,
@@ -76,7 +71,7 @@ export class SftpServerStack extends Stack {
       description: 'Security group for SFTP server',
     });
 
-    // In production always specify allowed IPs
+    // In production it's good to allow only specific IP addresses
     if (props.allowedIps) {
       props.allowedIps.forEach((ip) => {
         sg.addIngressRule(ec2.Peer.ipv4(ip), ec2.Port.tcp(22), 'Allow SSH inbound');
@@ -85,19 +80,17 @@ export class SftpServerStack extends Stack {
       sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH inbound');
     }
 
-    const eip1 = new ec2.CfnEIP(this, 'SftpEIP1', {
+    // Create as many Elastic IP addresses as we have availability zones
+    const eips = vpc.publicSubnets.map((_, index) => new ec2.CfnEIP(this, `SftpEIP${index + 1}`, {
       domain: 'vpc',
-    });
-    const eip2 = new ec2.CfnEIP(this, 'SftpEIP2', {
-      domain: 'vpc',
-    });
+    }));
 
     const server = new transfer.CfnServer(this, 'SFTPServer', {
       endpointDetails: {
         securityGroupIds: [sg.securityGroupId],
         vpcId: vpc.vpcId,
         subnetIds: vpc.publicSubnets.map((subnet) => subnet.subnetId),
-        addressAllocationIds: [eip1.attrAllocationId, eip2.attrAllocationId],
+        addressAllocationIds: eips.map((eip) => eip.attrAllocationId),
       },
       identityProviderType: 'SERVICE_MANAGED',
       endpointType: 'VPC',
@@ -111,7 +104,7 @@ export class SftpServerStack extends Stack {
       managedPolicyName: 'SftpAccessPolicy',
       description: 'SFTP access policy',
     });
-    this.incomingDataBucket.grantReadWrite(sftpAccessPolicy);
+    props.dataBucket.grantReadWrite(sftpAccessPolicy);
 
     const sftpUserAccessRole = new iam.Role(this, 'SftpAccessRole', {
       assumedBy: new iam.ServicePrincipal('transfer.amazonaws.com'),
@@ -121,32 +114,41 @@ export class SftpServerStack extends Stack {
       ],
     });
 
+    const logGroup = new logs.LogGroup(this, 'SftpLogGroup', {
+      logGroupName: `/aws/transfer/${server.attrServerId}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      retention: logs.RetentionDays.ONE_MONTH,
+    });
+
+    // Configure user which has access to the S3 bucket
+    // https://docs.aws.amazon.com/transfer/latest/userguide/service-managed-users.html
     new transfer.CfnUser(this, 'SFTPUser', {
       serverId: server.attrServerId,
-      homeDirectory: `/${this.incomingDataBucket.bucketName}/incoming-data`,
+      homeDirectory: `/${props.dataBucket.bucketName}/incoming-data`,
       role: sftpUserAccessRole.roleArn,
       userName: props.userName,
       sshPublicKeys: props.userPublicKeys,
     });
 
+    // Metric filter for recognizing two types of errors in the SFTP logs
     const metricFilter = new logs.MetricFilter(this, 'MetricFilter', {
-      logGroup: logs.LogGroup.fromLogGroupName(this, 'LogGroup', `/aws/transfer/${server.attrServerId}`),
+      logGroup,
       metricNamespace: 'SftpServer',
       metricName: 'ErrorLog',
       filterPattern: logs.FilterPattern.anyTerm('ERRORS AUTH_FAILURE', 'ERROR Message'),
       metricValue: '1',
       unit: cw.Unit.COUNT,
     });
-    const metric = metricFilter.metric();
 
+    // Alarm if there are too many errors
     this.errorAlarm = new cw.Alarm(this, 'AlarmMetricFilter', {
       alarmDescription: 'Alarm if there are too many errors in the logs',
-      metric,
+      metric: metricFilter.metric(),
       threshold: 1,
       evaluationPeriods: 5,
       datapointsToAlarm: 1,
     });
 
-    // TODO Add alarm action if required
+    // TODO Add alarm action to notify administrators or perform other actions
   }
 }
--- typescript/aws-transfer-sftp-server/main.ts
+++ typescript/aws-transfer-sftp-server/main.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import {
+  aws_s3 as s3,
+} from 'aws-cdk-lib';
 import { SftpServerStack, SftpServerStackProps } from './aws-transfer-sftp-server';
 
 // Follow the setup process at https://docs.aws.amazon.com/cdk/v2/guide/environments.html
@@ -14,13 +17,29 @@ const app = new cdk.App();
 
 // Use CDK Context to get configuration variables
 // https://docs.aws.amazon.com/cdk/v2/guide/context.html
-const userPublicKeys = (app.node.getContext('userPublicKeys') as string).split(',');
+const publicKeysContext = app.node.tryGetContext('userPublicKeys') as string | undefined;
+if (!publicKeysContext) {
+  console.error('Parameter userPublicKeys missing');
+}
 const allowedIps = app.node.tryGetContext('allowedIps') as string | undefined;
+
+const bucketStack = new cdk.Stack(app, 'IncomingDataStack-dev', props);
+const bucket = new s3.Bucket(bucketStack, 'IncomingDataBucket', {
+  blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+  bucketName: `sftp-server-data-bucket-${props.env?.account}-${props.env?.region}`,
+  encryption: s3.BucketEncryption.KMS_MANAGED,
+  enforceSSL: true,
+  // Do not use for production!
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+
+const userPublicKeys = publicKeysContext ? publicKeysContext.split(',') : [];
 
 // Different properties for dev/test environment
 const sftpPropsDev: SftpServerStackProps = {
   userName: 'sftp-user-dev',
   userPublicKeys,
+  dataBucket: bucket,
   ...props,
 };
 
@@ -29,10 +48,10 @@ const sftpProps: SftpServerStackProps = {
   userName: 'sftp-user',
   userPublicKeys,
   allowedIps: allowedIps ? allowedIps.split(',') : undefined,
+  dataBucket: bucket,
   ...props,
 };
 
 new SftpServerStack(app, 'SftpServerStack-dev', sftpPropsDev);
 
-// Enable if needed
-// new SftpServerStack(app, 'SftpServerStack-prod', sftpProps);
+new SftpServerStack(app, 'SftpServerStack-prod', sftpProps);
