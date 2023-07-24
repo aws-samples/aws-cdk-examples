30d08a4df2630fef96d06680d5b6e8996a875214
Pahud Hsieh <pahudnet@gmail.com>
2023-07-23 22:43:40 -0400

    Update build-typescript.sh

--- scripts/build-typescript.sh
+++ scripts/build-typescript.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
+# triggered by scripts/build-${buildlang}.sh $path2 where $path2 is the directory under typescripts
 set -euxo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
+projectname="$1"
 
-# Find and build all NPM projects
-for pkgJson in $(find typescript -name cdk.json | grep -v node_modules | sort); do
-    (
-        echo "=============================="
-        echo "building project: $(dirname $pkgJson)"
-        echo "=============================="
 
-        cd $(dirname $pkgJson)
-        if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
+echo "=============================="
+echo "building project: typescript/$projectname"
+echo "=============================="
 
-        rm -rf package-lock.json node_modules
-        npm install
-        npm run build
+cd "typescript/$projectname";
+yarn install --frozen-lockfile
+yarn build
+yarn test
+$scriptdir/synth.sh
 
-        $scriptdir/synth.sh
-    )
-done
+exit 0
+
+# # Find and build all NPM projects
+# for pkgJson in $(find typescript -name cdk.json | grep -v node_modules | sort); do
+#     (
+#         echo "=============================="
+#         echo "building project: $(dirname $pkgJson)"
+#         echo "=============================="
+
+#         cd $(dirname $pkgJson)
+#         if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
+
+#         rm -rf package-lock.json node_modules
+#         npm install
+#         npm run build
+
+#         $scriptdir/synth.sh
+#     )
+# done
