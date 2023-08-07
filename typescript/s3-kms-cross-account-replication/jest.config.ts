e81165d8662b4cf26ab11f4c4c6b357aab922a8c
Markus Lindqvist <mlindqv@amazon.com>
2023-08-07 19:13:03 +0300

    chore: Update Typescript to 5.1.6 and ts-node to 10.9.1 (#901)

---
+++ typescript/s3-kms-cross-account-replication/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
+};
+export default config;
