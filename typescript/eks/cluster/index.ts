99288741e1c4f1c76486e33f12a981905387e3a7
Michael Kaiser <mjkaiser@gmail.com>
2023-10-12 09:07:29 -0500

    Fix(ts:eks/cluster): Update versions and fix unsupported EKS

--- typescript/eks/cluster/index.ts
+++ typescript/eks/cluster/index.ts
@@ -3,13 +3,14 @@ import iam = require('aws-cdk-lib/aws-iam');
 import ec2 = require('aws-cdk-lib/aws-ec2');
 import eks = require('aws-cdk-lib/aws-eks');
 import cdk = require('aws-cdk-lib');
+import { KubectlLayer } from 'aws-cdk-lib/lambda-layer-kubectl';
 
 class EKSCluster extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'EKSVpc');  // Create a new VPC for our cluster
-    
+
     // IAM role for our EC2 worker nodes
     const workerRole = new iam.Role(this, 'EKSWorkerRole', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
@@ -18,7 +19,8 @@ class EKSCluster extends cdk.Stack {
     const eksCluster = new eks.Cluster(this, 'Cluster', {
       vpc: vpc,
       defaultCapacity: 0,  // we want to manage capacity our selves
-      version: eks.KubernetesVersion.V1_21,
+      version: eks.KubernetesVersion.V1_27,
+      kubectlLayer: new KubectlLayer(this, 'KubectlLayer'),
     });
 
     const onDemandASG = new autoscaling.AutoScalingGroup(this, 'OnDemandASG', {
@@ -26,9 +28,10 @@ class EKSCluster extends cdk.Stack {
       role: workerRole,
       minCapacity: 1,
       maxCapacity: 10,
+      allowAllOutbound: false,
       instanceType: new ec2.InstanceType('t3.medium'),
       machineImage: new eks.EksOptimizedImage({
-        kubernetesVersion: '1.21',
+        kubernetesVersion: eks.KubernetesVersion.V1_27.version,
         nodeType: eks.NodeType.STANDARD  // without this, incorrect SSM parameter for AMI is resolved
       }),
       updatePolicy: autoscaling.UpdatePolicy.rollingUpdate()
