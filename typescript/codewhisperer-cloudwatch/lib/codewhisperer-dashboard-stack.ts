1d5acb84dc632878122afc2e3e3e2f1d42244ad5
Eric Beard <ericzbeard@gmail.com>
2023-10-23 14:48:42 -0700

    Added line rates

--- typescript/codewhisperer-cloudwatch/lib/codewhisperer-dashboard-stack.ts
+++ typescript/codewhisperer-cloudwatch/lib/codewhisperer-dashboard-stack.ts
@@ -9,44 +9,45 @@ export class CodewhispererDashboardStack extends cdk.Stack {
 
     // Set up a CloudWatch dashboard that shows CodeWhisperer metrics
     const dashboard = new Dashboard(this, "cw-dashboard", {
-        dashboardName: "CodeWhisperer-Metrics"
+        dashboardName: "CodeWhisperer-AcceptRates"
     });
 
     // Replicate all widgets for these languages
     const languages = ["Python", "Java", "JavaScript", "TypeScript"];
 
+    function createInvocationMetric(
+        language: string, completionType: string, accept: boolean) {
+
+        return new Metric({
+            metricName: "InvocationCount", 
+            namespace: "AWS/CodeWhisperer", 
+            dimensionsMap: {
+                "SuggestionState": accept ? "ACCEPT" : "REJECT", 
+                "CompletionType": completionType, 
+                "ProgrammingLanguage": language.toLowerCase() 
+            },
+            statistic: "Sum", 
+            label: completionType + " Accept", 
+            period: Duration.seconds(300)
+        });
+    }
+
     for (const language of languages) {
 
-        // Create a metric for accept
-        const m1 = new Metric({
-            metricName: "InvocationCount", 
-            namespace: "AWS/CodeWhisperer", 
-            dimensionsMap: {
-                "SuggestionState": "ACCEPT", 
-                "CompletionType": "BLOCK", 
-                "ProgrammingLanguage": language.toLowerCase() 
-            },
-            statistic: "Sum", 
-            label: "Accept", 
-            period: Duration.seconds(300)
-        });
+        // Create a metric for block accept
+        const m1 = createInvocationMetric(language, "BLOCK", true)
 
-        // Create a metric for reject
-        const m2 = new Metric({
-            metricName: "InvocationCount", 
-            namespace: "AWS/CodeWhisperer", 
-            dimensionsMap: {
-                "SuggestionState": "REJECT", 
-                "CompletionType": "BLOCK", 
-                "ProgrammingLanguage": language.toLowerCase() 
-            },
-            statistic: "Sum", 
-            label: "Reject", 
-            period: Duration.seconds(300)
-        });
+        // Create a metric for block reject
+        const m2 = createInvocationMetric(language, "BLOCK", false)
+
+        // Create a metric for line accept
+        const m3 = createInvocationMetric(language, "LINE", true)
+
+        // Create a metric for line reject
+        const m4 = createInvocationMetric(language, "LINE", false)
 
         // Create an expression for accept rate 
-        const rate = new MathExpression({
+        const blockRate = new MathExpression({
             label: "Block Accept Rate", 
             expression: "100*m1/(m1+m2)",
             usingMetrics: {
@@ -55,14 +56,44 @@ export class CodewhispererDashboardStack extends cdk.Stack {
             }
         });
 
-        // Create a widget to display accept, reject, and rate
-        const w1 = new GraphWidget({
-            width: 6, 
-            title: language, 
-            left: [ m1, m2, rate ]
+        const lineRate = new MathExpression({
+            label: "Line Accept Rate", 
+            expression: "100*m3/(m3+m4)",
+            usingMetrics: {
+                "m3": m3, 
+                "m4": m4
+            }
         });
 
-        dashboard.addWidgets(w1);
+        // Create a widget to display block accepts and rejects
+        const w1 = new GraphWidget({
+            width: 6, 
+            title: language + " Block Accept/Reject", 
+            left: [ m1, m2 ]
+        });
+
+        // Create a widget to display block accept rate
+        const w2 = new GraphWidget({
+            width: 6, 
+            title: language + " Block Accept Rate", 
+            left: [ blockRate ]
+        });
+
+        // Create a widget to display line accepts and rejects
+        const w3 = new GraphWidget({
+            width: 6, 
+            title: language + " Line Accept/Reject", 
+            left: [ m3, m4 ]
+        });
+
+        // Create a widget to display line accept rate
+        const w4 = new GraphWidget({
+            width: 6, 
+            title: language + " Line Accept Rate", 
+            left: [ lineRate ]
+        });
+
+        dashboard.addWidgets(w1, w2, w3, w4);
     }
   }
 }
