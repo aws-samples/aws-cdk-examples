b452913d833c04098d0aac911e44bfa5784df200
Rucha Deshpande <deshruch@amazon.com>
2023-11-07 09:29:59 -0600

    initial commit

---
+++ typescript/opensearch/os_vpc_provision/lib/os_vpc_provision-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class OsVpcProvisionStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'OsVpcProvisionQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
