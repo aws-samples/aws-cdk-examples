3a8db8802d35343ffe0f3649f74591dbf88da942
Cody Currie <cjcurrie@amazon.com>
2023-12-02 14:34:02 -0700

    added example for java backup s3

---
+++ java/backup-s3/src/main/java/com/myorg/BackupS3App.java
@@ -0,0 +1,15 @@
+package com.myorg;
+// package com.amazonaws.cdk;
+
+import software.amazon.awscdk.App;
+
+public class BackupS3App {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new BackupS3Stack(app, "BackupS3Stack");
+
+        app.synth();
+    }
+}
+
