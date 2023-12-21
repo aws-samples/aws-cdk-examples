6d8e0abb5947318e51b7c250615dddd8e01946f3
Tommy Whelan <whelat@amazon.com>
2023-12-21 17:22:51 -0500

    fix app.py

--- python/ssh-into-emr-cluster/app.py
+++ python/ssh-into-emr-cluster/app.py
@@ -9,7 +9,7 @@ EmrPatternStack(
     app,
     "EmrPatternStack",
     env=cdk.Environment(
-        account="416880646752",
+        account="222222222222",
         region="us-east-1"
     ),
     ssh_origin_ip=None  # Leave as is or pass in your IP as a string: "12.34.56.78"
