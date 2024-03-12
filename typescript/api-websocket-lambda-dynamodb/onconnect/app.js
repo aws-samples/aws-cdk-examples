eb105d1d84a5f5acdb2510861e99fbfd640ed2d0
Cameron Tod <139489+cam8001@users.noreply.github.com>
2024-03-13 00:07:28 +1300

    Updated onconnect to aws-sdk v3

--- typescript/api-websocket-lambda-dynamodb/onconnect/app.js
+++ typescript/api-websocket-lambda-dynamodb/onconnect/app.js
@@ -1,20 +1,23 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+// @see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_dynamodb_code_examples.html
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const AWS = require('aws-sdk');
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
-
-exports.handler = async event => {
-  const putParams = {
+export const handler = async(event, context) => {
+  const command = new PutCommand({
     TableName: process.env.TABLE_NAME,
     Item: {
       connectionId: event.requestContext.connectionId
     }
-  };
+  });
 
   try {
-    await ddb.put(putParams).promise();
+   const response = await docClient.send(command);
+   console.log(response);
   } catch (err) {
     return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
   }
