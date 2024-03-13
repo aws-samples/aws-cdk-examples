89330fd8d622e68ed1acea4fd85e4bc4f775ec4f
Cameron Tod <139489+cam8001@users.noreply.github.com>
2024-03-14 12:50:03 +1300

    Updated code for message lambda to aws-sdk v3.

--- typescript/api-websocket-lambda-dynamodb/sendmessage/app.js
+++ typescript/api-websocket-lambda-dynamodb/sendmessage/app.js
@@ -1,45 +1,54 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+// @see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_dynamodb_code_examples.html
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, DeleteCommand } from '@aws-sdk/lib-dynamodb'
+// @see https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/apigatewaymanagementapi/command/PostToConnectionCommand/
+import { ApiGatewayManagementApiClient, GoneException, PostToConnectionCommand } from "@aws-sdk/client-apigatewaymanagementapi";
 
-const AWS = require('aws-sdk');
-
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const ddb = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(ddb);
 
 const { TABLE_NAME } = process.env;
 
-exports.handler = async event => {
-  let connectionData;
-  
+export const handler = async (event, context) => {
+  let connectionDataResponse;
+
+  // Get a list of connections to our API that have been recorded in DynamoDB.
   try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+    const command = new ScanCommand({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' });
+    connectionDataResponse = await docClient.send(command);
   } catch (e) {
     return { statusCode: 500, body: e.stack };
   }
-  
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  console.info(`Found ${connectionDataResponse.Items.length} connections`, connectionDataResponse.Items);
+
+  // @see https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-how-to-call-websocket-api-connections.html
+  const callbackUrl = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
+  const apigwManagementApi = new ApiGatewayManagementApiClient({
+    endpoint: callbackUrl
   });
-  
+
   const postData = JSON.parse(event.body).data;
-  
-  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+  console.log(`Will send POST data to APIGW: ${postData}`);
+
+  for (const connection of connectionDataResponse.Items) {
+    const connectionId = connection.connectionId.S;
+    console.log(`Trying to send data for connection: ${connectionId}`);
     try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      const command = new PostToConnectionCommand({ ConnectionId: connectionId, Data: postData });
+      await apigwManagementApi.send(command);
+      console.log(`Sent ${postData} to ${connectionId}`);
     } catch (e) {
-      if (e.statusCode === 410) {
+      if (e instanceof GoneException) {
         console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+        const deleteCommand = new DeleteCommand({ TableName: TABLE_NAME, Key: { connectionId } });
+        await docClient.send(deleteCommand);
       } else {
+        console.error(`Received error when sending to ${callbackUrl}`, e);
         throw e;
       }
     }
-  });
-  
-  try {
-    await Promise.all(postCalls);
-  } catch (e) {
-    return { statusCode: 500, body: e.stack };
   }
 
   return { statusCode: 200, body: 'Data sent.' };
