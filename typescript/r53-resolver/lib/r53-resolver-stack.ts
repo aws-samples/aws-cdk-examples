2cab985b101b4042e1fdf4711e0f9af34c912dea
Cameron Tod <139489+cam8001@users.noreply.github.com>
2024-03-21 11:47:52 +1300

    Initial deployable stack with DNS firewall

--- typescript/r53-resolver/lib/r53-resolver-stack.ts
+++ typescript/r53-resolver/lib/r53-resolver-stack.ts
@@ -1,16 +1,51 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import * as route53resolver from "@aws-cdk/aws-route53resolver-alpha";
+import { R53ResolverVPC } from "./vpc";
+import * as path from "path";
 
 export class R53ResolverStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // @see https://docs.aws.amazon.com/cdk/api/v2/docs/aws-route53resolver-alpha-readme.html
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'R53ResolverQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Example of directly including domains in a list.
+    const directBlockList = new route53resolver.FirewallDomainList(
+      this,
+      "BlockList",
+      {
+        domains: route53resolver.FirewallDomains.fromList([
+          "example.com",
+          "example.net",
+        ]),
+      }
+    );
+
+    // Create a rule group to handle our list of blocked domains.
+    const ruleGroup = new route53resolver.FirewallRuleGroup(
+      this,
+      "BlockListRuleGroup",
+      {
+        rules: [
+          {
+            priority: 10,
+            firewallDomainList: directBlockList,
+            action: route53resolver.FirewallRuleAction.block(),
+          },
+        ],
+      }
+    );
+
+    // Create a VPC for us to deploy into.
+    const vpc = new R53ResolverVPC(this, "R53ResolverTestVPC");
+
+    new cdk.CfnOutput(this, "VPCId", { value: vpc.vpc.vpcId });
+
+    ruleGroup.associate("DirectBlockListRuleGroupAssociation", {
+      priority: 101,
+      vpc: vpc.vpc,
+    });
   }
 }
