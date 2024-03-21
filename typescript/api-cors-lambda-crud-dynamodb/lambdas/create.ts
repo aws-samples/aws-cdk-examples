de1e203a23d39a3bec3a43480a0533e717928f6f
Michael Kaiser <kaiser@kaiz.io>
2024-03-21 10:55:06 -0500

    Fix(typescript/api-cors-lambda-crud-dynamodb): Update to nodejs 20

--- typescript/api-cors-lambda-crud-dynamodb/lambdas/create.ts
+++ typescript/api-cors-lambda-crud-dynamodb/lambdas/create.ts
@@ -1,10 +1,11 @@
-import * as AWS from 'aws-sdk';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { v4 as uuidv4 } from 'uuid';
 
 const TABLE_NAME = process.env.TABLE_NAME || '';
 const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
 
-const db = new AWS.DynamoDB.DocumentClient();
+const db = DynamoDBDocument.from(new DynamoDB());
 
 const RESERVED_RESPONSE = `Error: You're using AWS reserved keywords as attributes`,
   DYNAMODB_EXECUTION_ERROR = `Error: Execution update, caused a Dynamodb error, please take a look at your CloudWatch Logs.`;
@@ -22,7 +23,7 @@ export const handler = async (event: any = {}): Promise<any> => {
   };
 
   try {
-    await db.put(params).promise();
+    await db.put(params);
     return { statusCode: 201, body: '' };
   } catch (dbError) {
     const errorResponse = dbError.code === 'ValidationException' && dbError.message.includes('reserved keyword') ?
