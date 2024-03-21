9afdbcd21d02a118e01a5c880a3fa5ec65023f29
Cameron Tod <139489+cam8001@users.noreply.github.com>
2024-03-22 12:03:22 +1300

    Added outbound resolver endpoint

--- typescript/r53-resolver/lib/r53-resolver-stack.ts
+++ typescript/r53-resolver/lib/r53-resolver-stack.ts
@@ -1,22 +1,31 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
-import * as route53resolver from "@aws-cdk/aws-route53resolver-alpha";
+import * as route53resolverAlpha from "@aws-cdk/aws-route53resolver-alpha";
+import * as route53resolver from "aws-cdk-lib/aws-route53resolver";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { R53ResolverVPC } from "./vpc";
-import * as path from "path";
 
 export class R53ResolverStack extends cdk.Stack {
+  protected targetVpc: Vpc;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // Create a VPC for us to deploy into.
+    this.targetVpc = new R53ResolverVPC(this, "R53ResolverTestVPC").vpc;
+    this.createDnsFirewall();
+    this.createOutboundEndpoint();
+  }
 
+  createDnsFirewall() {
     // @see https://docs.aws.amazon.com/cdk/api/v2/docs/aws-route53resolver-alpha-readme.html
 
     // Example of directly including domains in a list.
-    const directBlockList = new route53resolver.FirewallDomainList(
+    const directBlockList = new route53resolverAlpha.FirewallDomainList(
       this,
       "BlockList",
       {
-        domains: route53resolver.FirewallDomains.fromList([
+        domains: route53resolverAlpha.FirewallDomains.fromList([
           "example.com",
           "example.net",
         ]),
@@ -24,7 +33,7 @@ export class R53ResolverStack extends cdk.Stack {
     );
 
     // Create a rule group to handle our list of blocked domains.
-    const ruleGroup = new route53resolver.FirewallRuleGroup(
+    const ruleGroup = new route53resolverAlpha.FirewallRuleGroup(
       this,
       "BlockListRuleGroup",
       {
@@ -32,20 +41,91 @@ export class R53ResolverStack extends cdk.Stack {
           {
             priority: 10,
             firewallDomainList: directBlockList,
-            action: route53resolver.FirewallRuleAction.block(),
+            action: route53resolverAlpha.FirewallRuleAction.block(),
           },
         ],
       }
     );
 
-    // Create a VPC for us to deploy into.
-    const vpc = new R53ResolverVPC(this, "R53ResolverTestVPC");
-
-    new cdk.CfnOutput(this, "VPCId", { value: vpc.vpc.vpcId });
-
     ruleGroup.associate("DirectBlockListRuleGroupAssociation", {
       priority: 101,
-      vpc: vpc.vpc,
+      vpc: this.targetVpc,
+    });
+
+    const consoleBaseUrl = `https://console.aws.amazon.com/`;
+    const region = cdk.Stack.of(this).region;
+    const vpcBaseUrl = `${consoleBaseUrl}vpc/home?region=${region}`;
+
+    // Output details of the VPC.
+    new cdk.CfnOutput(this, "VPC-Id", { value: this.targetVpc.vpcId });
+    new cdk.CfnOutput(this, "VPC-Link", {
+      value: `https://console.aws.amazon.com/vpcconsole/home/#VpcDetails:VpcId=${this.targetVpc.vpcId}`,
+    });
+
+    // Output details of the DNS firewall.
+    new cdk.CfnOutput(this, "DNS-Firewall-Id", {
+      value: ruleGroup.firewallRuleGroupId,
+    });
+    new cdk.CfnOutput(this, "DNS-Firewall-Link", {
+      value: `${vpcBaseUrl}#DNSFirewallRuleGroupDetails:RulegroupId=${ruleGroup.firewallRuleGroupId}`,
+    });
+  }
+
+  /**
+   * Creates an outbound endpoint, for resources inside the VPC to use as a DNS server.
+   */
+  createOutboundEndpoint() {
+    const sgOutboundEndpoint = new ec2.SecurityGroup(
+      this,
+      "outbound-endpoint-sg",
+      {
+        vpc: this.targetVpc,
+        allowAllOutbound: true,
+        description: "Security group for outbound endpoint",
+      }
+    );
+
+    sgOutboundEndpoint.addIngressRule(
+      ec2.Peer.ipv4(this.targetVpc.vpcCidrBlock),
+      ec2.Port.tcp(53)
+    );
+
+    sgOutboundEndpoint.addIngressRule(
+      ec2.Peer.ipv4(this.targetVpc.vpcCidrBlock),
+      ec2.Port.udp(53)
+    );
+
+    const subnets = this.targetVpc.selectSubnets({
+      subnetType: cdk.aws_ec2.SubnetType.PRIVATE_ISOLATED,
+    });
+    let subnetList = [];
+    let ipAddresses = [];
+    for (const subnet of subnets.subnets) {
+      subnetList.push(subnet.subnetId);
+      ipAddresses.push(subnet);
+    }
+
+    // @see https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_route53resolver.CfnResolverEndpoint.html
+    // @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-route53resolver-resolverendpoint.html
+
+    const outboundEndpoint = new route53resolver.CfnResolverEndpoint(
+      this,
+      "OutboundEndpoint",
+      {
+        direction: "OUTBOUND",
+        // Two IP addresses for the endpoint are required.
+        ipAddresses: [{ subnetId: subnetList[0] }, { subnetId: subnetList[1] }],
+        securityGroupIds: [sgOutboundEndpoint.securityGroupId],
+      }
+    );
+
+    new cdk.CfnOutput(this, "OutboundEndpointId", {
+      value: outboundEndpoint.attrResolverEndpointId,
+    });
+
+    const region = cdk.Stack.of(this).region;
+    new cdk.CfnOutput(this, "OutboundEndpointLink", {
+      value: `https://${region}.console.aws.amazon.com/route53resolver/home?region=${region}#/endpoint/${outboundEndpoint.attrResolverEndpointId}`,
     });
   }
 }
