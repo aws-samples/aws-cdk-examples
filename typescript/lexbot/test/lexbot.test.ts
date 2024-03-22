d1b62fd841838e60053c2d132b55bd7e2291ab8a
Rushali Dantrey <111036690+rushali-aws@users.noreply.github.com>
2024-03-22 11:09:12 +0530

    Add files via upload

---
+++ typescript/lexbot/test/lexbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Lexbot from '../lib/lexbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lexbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Lexbot.LexbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
