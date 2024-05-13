b321fef8cff3dfd851f54804690991f97b61d075
Michael Kaiser <1553631+kaiz-io@users.noreply.github.com>
2024-05-13 00:59:19 -0500

    Fix: GH workflows (#6)
    * Fix: GH workflows

--- scripts/build-java.sh
+++ scripts/build-java.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 set -euxo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
+projFile=$1
 
-# install CDK CLI from npm, so that npx can find it later
-cd $scriptdir/../java
-npm install
+echo "=============================="
+echo "building project: $(dirname $projFile)"
+echo "=============================="
 
-# Find and build all Maven projects
-for pomFile in $(find $scriptdir/../java -name pom.xml | grep -v node_modules); do
-    (
-        echo "=============================="
-        echo "building project: $(dirname $pomFile)"
-        echo "=============================="
+cd $scriptdir/../$(dirname $projFile)
+if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
 
-        cd $(dirname $pomFile)
-        if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
+mvn -q compile test
 
-        mvn -q compile test
-
-        $scriptdir/synth.sh
-    )
-done
+$scriptdir/synth.sh
