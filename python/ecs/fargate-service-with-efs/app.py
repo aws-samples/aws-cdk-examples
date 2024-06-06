610c14498cb6756712504ca91fb47e2585732514
Bruce McLeod <brucemcl@amazon.com>
2024-06-06 13:32:05 +1000

    Added ReadMe, updated aws-cdk-lib, cleaned up and commented app,py

--- python/ecs/fargate-service-with-efs/app.py
+++ python/ecs/fargate-service-with-efs/app.py
@@ -16,47 +16,50 @@ class FargateServiceWithEfs(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, *kwargs)
 
-        PREFIX      = 'My'
+        PREFIX      = 'efs-sample-'
         APP_PATH    = '/var/www/'
-        VOLUME_NAME = 'ecspattern-efs-volume'
+        VOLUME_NAME = 'cdk-ecs-sample-efs-volume'
 
         vpc = ec2.Vpc(
-            self, PREFIX+'Vpc',
+            self, PREFIX + 'Vpc',
             max_azs=2
         )
 
-        cluster = ecs.Cluster(
-            self, PREFIX+'Cluster',
+        ecs_cluster = ecs.Cluster(
+            self, PREFIX + 'Cluster',
             vpc=vpc,
         )
 
-        # EFS File System
+        # Create an Amazon Elastic File System (EFS), with the logical ID CDK-efs-sample-EFS
         file_system = efs.FileSystem(
-            self, PREFIX+'EFS',
+            self, PREFIX + 'EFS',
             vpc=vpc,
             lifecycle_policy=efs.LifecyclePolicy.AFTER_14_DAYS,
             performance_mode=efs.PerformanceMode.GENERAL_PURPOSE,
         )
 
-        ap = efs.AccessPoint(
-            self, PREFIX+'AccessPoint',
+        # Create an Access Point for the EFS, with the logical ID CDK-efs-sample-AccessPoint
+        access_point = efs.AccessPoint(
+            self, PREFIX + 'AccessPoint',
             file_system=file_system,
         )
 
+        # Create a new EFS volume configuration for the ECS Task
         efs_volume_configuration = ecs.EfsVolumeConfiguration(
             file_system_id=file_system.file_system_id,
 
-            # the properties below are optional
+            # The logical ID of the Access Point to use.
+            # This is a string, not an ARN.
             authorization_config=ecs.AuthorizationConfig(
-                access_point_id=ap.access_point_id,
+                access_point_id=access_point.access_point_id,
                 iam='ENABLED',
             ),
             transit_encryption='ENABLED',
         )
 
-        # ECS Task Role:
+        # Create a new IAM Role for the ECS Task
         task_role = iam.Role (
-            self, PREFIX+'EcsTaskRole',
+            self, PREFIX + 'EcsTaskRole',
             assumed_by=iam.ServicePrincipal('ecs-tasks.amazonaws.com').with_conditions({
                 "StringEquals": {
                     "aws:SourceAccount": Stack.of(self).account
@@ -66,8 +69,10 @@ class FargateServiceWithEfs(Stack):
                 },
             }),
         )
+
+        # Attach a managed policy to the IAM Role
         task_role.attach_inline_policy(
-            iam.Policy(self, PREFIX+'Policy',
+            iam.Policy(self, PREFIX +'Policy',
                 statements=[
                     iam.PolicyStatement(
                         effect=iam.Effect.ALLOW,
@@ -92,47 +97,55 @@ class FargateServiceWithEfs(Stack):
             )
         )
 
-        # ECS Task Definition
-        task_def = ecs.FargateTaskDefinition(
-            self, PREFIX+'FargateTaskDef',
+        # Create a new Fargate Task Definition
+        task_definition = ecs.FargateTaskDefinition(
+            self, PREFIX + 'FargateTaskDef',
             task_role=task_role,
         )
 
-        task_def.add_volume(
+        # Add a new volume to the Fargate Task Definition
+        task_definition.add_volume(
             name=VOLUME_NAME,
             efs_volume_configuration=efs_volume_configuration,
         )
 
+        # Add a new container to the Fargate Task Definition
         mount_point = ecs.MountPoint(
             container_path=APP_PATH+VOLUME_NAME,
             source_volume=VOLUME_NAME,
             read_only=False,
         )
 
+        # Add a new port mapping to the Fargate Task Definition
         port_mapping = ecs.PortMapping(
             container_port=80,
             host_port=80,
             protocol=ecs.Protocol.TCP,
         )
 
+        # Add a new container to the Fargate Task Definition
         container = ecs.ContainerDefinition(
-            self, 'ecs-sample',
-            task_definition=task_def,
+            self, 'ecs-cdk-sample-container',
+            task_definition=task_definition,
             image=ecs.ContainerImage.from_registry('amazon/amazon-ecs-sample'),
             logging=ecs.LogDrivers.aws_logs(
-                stream_prefix='myecs', 
+                stream_prefix='cdk-ecs-sample', 
                 log_retention=logs.RetentionDays.ONE_MONTH,
             )
         )
+
+        # Add a new volume to the Fargate Task Definition
         container.add_mount_points(mount_point),
+
+        # Add a new port mapping to the Fargate Task Definition
         container.add_port_mappings(port_mapping),
 
-        # ECS Patterns - Application LB Fargate Service
+        # Create a new Fargate Service
         fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
-            self, PREFIX+'Service',
-            cluster=cluster,
+            self, PREFIX + 'Service',
+            cluster=ecs_cluster,
             desired_count=1,
-            task_definition=task_def,
+            task_definition=task_definition,
             task_subnets=ec2.SubnetSelection(
                 subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
             ),
@@ -140,23 +153,32 @@ class FargateServiceWithEfs(Stack):
             public_load_balancer=True,
             enable_execute_command=True,
             enable_ecs_managed_tags=True,
+
         )
+
+        # Allow the ECS Service to connect to the EFS
         fargate_service.service.connections.allow_from(file_system, ec2.Port.tcp(2049)),
+
+        # Allow the EFS to connect to the ECS Service
         fargate_service.service.connections.allow_to(file_system, ec2.Port.tcp(2049)),
 
+        # Create a new Auto Scaling Policy for the ECS Service
         scalable_target = fargate_service.service.auto_scale_task_count(
             min_capacity=1,
             max_capacity=20,
         )
 
+        # Create a new Auto Scaling Policy for the ECS Service
         scalable_target.scale_on_cpu_utilization("CpuScaling",
             target_utilization_percent=50,
         )
 
+        # Create a new Auto Scaling Policy for the ECS Service
         scalable_target.scale_on_memory_utilization("MemoryScaling",
             target_utilization_percent=50,
         )
 
-app = App()
-FargateServiceWithEfs(app, "aws-fargate-service-with-efs")
-app.synth()
+# Create the new CDK Application
+cdk_application = App()
+FargateServiceWithEfs(cdk_application, "AWS-CDK-Fargate-")
+cdk_application.synth()
