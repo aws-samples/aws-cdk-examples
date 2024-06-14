3531d282da465dbc1b1c896db8b85d404b2983e6
Haresh Kanagasabapathi <hareshkb@amazon.com>
2024-06-14 10:45:17 -0700

    Issue-1027: created efs access point, fargate service, ecs task definition, task role

--- python/ecs/fargate-service-with-efs/app.py
+++ python/ecs/fargate-service-with-efs/app.py
@@ -7,14 +7,14 @@ from aws_cdk import (
     App, CfnOutput, Duration, Stack
 )
 from constructs import Construct
-class FargateEFS(Stack):
+class FargateEfs(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Create VPC with 2 AZ and a Fargate Cluster
-        vpc = ec2.Vpc(
-            self, "MyFargateVpc",
+        my_fargate_vpc = ec2.Vpc(
+            self, "my_fargate_vpc",
             max_azs=2
         )
 
@@ -22,7 +22,7 @@ class FargateEFS(Stack):
         my_file_system_policy = iam.PolicyDocument(
             statements=[iam.PolicyStatement(
                 actions=["elasticfilesystem:ClientMount"],
-                principals=[iam.AccountRootPrincipal()],
+                principals=[iam.AnyPrincipal()],
                 resources=["*"],
                 conditions={
                     "Bool": {
@@ -32,44 +32,95 @@ class FargateEFS(Stack):
             )]
         )
 
-        # Create EFS
-        my_file_system = efs.FileSystem(self, "My_Fargate_File_System",
-            vpc=vpc,
+        # Create EFS in MyFargateVpc
+        my_file_system = efs.FileSystem(self, "my_fargate_file_system",
+            vpc=my_fargate_vpc,
             encrypted=True,
             file_system_policy=my_file_system_policy,
             lifecycle_policy=efs.LifecyclePolicy.AFTER_14_DAYS,
             performance_mode=efs.PerformanceMode.GENERAL_PURPOSE,
             throughput_mode=efs.ThroughputMode.BURSTING,
         )
+
+        # Create an access point for the file system
+        access_point = my_file_system.add_access_point(
+            "MyAccessPoint",
+            path="/uploads",
+            create_acl=efs.Acl(
+                owner_uid="1001",
+                owner_gid="1001",
+                permissions="0755"
+            ),
+            posix_user=efs.PosixUser(
+                uid="1001",
+                gid="1001"
+            )
+        )
+
+        # Output the access point ID and ARN
+
+        CfnOutput(
+            self, "AccessPointId",
+            value=access_point.access_point_id,
+            description="Access Point ID"
+        )
+
+        CfnOutput(
+            self, "AccessPointArn",
+            value=access_point.access_point_arn,
+            description="Access Point ARN"
+        )
+
+        # Create an ECS cluster with Fargate launch type
+        cluster = ecs.Cluster(
+            self, "ECS-EFS-Cluster",
+            vpc=my_fargate_vpc,
+            cluster_name="my_ecs_fargate_efs_cluster"
+        )
         
-        # Create Access point
-        my_file_system.add_access_point("AccessPoint", "path=/uploads")    
+        # Define the task definition
+        task_definition = ecs.FargateTaskDefinition(self, "MyTaskDefinition")
+        
+        # Create a task execution role with EFS permissions
+        my_task_execution_role = iam.Role(self, "TaskExecutionRole", assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"))
+        my_file_system.grant_root_access(my_task_execution_role)
 
-        image=ecs.ContainerImage.fromRegistry("coderaiser/cloudcmd"),
+        # Add a container to the task definition
+        container = task_definition.add_container(
+            "WebContainer",
+            image=ecs.ContainerImage.from_registry("coderaiser/cloudcmd"),
+            port_mappings=[ecs.PortMapping(container_port=8000)],
+            memory_limit_mib=512,
+            cpu=1,
+        )
+        
 
-        # Create ECS cluster with fargate service  
-        fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(self, "MyFargateService",
-            cluster=ecs.Cluster(
-                self, "ECS-EFS-Cluster",
-                vpc=vpc
-            ),
-            task_image_options=ecs_patterns.NetworkLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_registry("coderaiser/cloudcmd"),
-                container_port=8080,
-                enable_logging=True,
-                environment={
-                    "PORT": "8080",
-                    "VIRTUAL_HOST": "cloudcmd.local"
-                },
-                file_system_configs=[
-                    ecs.FileSystemConfig(
-                        file_system=my_file_system,
-                        mount_path="/uploads",
-                        read_only=False
-                    )
-                ]
-            ),
-            memory_limit_mib=1024,
-            cpu=512,
+        # Create a Fargate service within the cluster
+        fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(
+            self, "MyFargateService",
+            cluster=cluster,
+            task_definition=task_definition,
             desired_count=2
         )
+
+        # Mount the EFS filesystem to the containers
+        fargate_service.service.task_definition.default_container.add_mount_points(
+            ecs.MountPoint(
+                container_path="/uploads",
+                source_volume="uploads",
+                read_only=False
+            )
+        )
+
+
+        # Output the DNS name of the load balancer
+        # CfnOutput(
+        #     self, "LoadBalancerDNS",
+        #     value=fargate_service.load_balancer.load_balancer_dns_name,
+        #     description="DNS of the load balancer"
+        # )
+
+
+app = App()
+FargateEfs(app, "aws-fargate-application-autoscaling")
+app.synth()
