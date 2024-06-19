c4be1bcaf1bf85a3cb22384b0a4777867f356dff
Simon Carter <sicart@amazon.com>
2024-06-19 11:50:02 +1000

    Converted all constants to strings

--- typescript/serverless-multi-region-tokenizer/lib/constants.ts
+++ typescript/serverless-multi-region-tokenizer/lib/constants.ts
@@ -4,15 +4,15 @@ SPDX-License-Identifier: MIT-0
 */
 
 export namespace Constants {
-    export const ddbTokenTable = "sensitive-data";
+    export const ddbTokenTable : string = "sensitive-data";
     //this is the partition key
-    export const ddbPartitionKey = "tokenId";
+    export const ddbPartitionKey : string = "tokenId";
 
-    export const kmsMultiRegionKeyId = "mrk-92f783ae30e84899926644ce3d7e62d6";
-    export const kmsMultiRegionKeyAlias = "sensitive-data-cmk";
+    export const kmsMultiRegionKeyId : string = "mrk-92f783ae30e84899926644ce3d7e62d6";
+    export const kmsMultiRegionKeyAlias : string = "sensitive-data-cmk";
 
     //export const replicaRegion = 'ap-southeast-1';  //Singapore, note that primary region is set by CDK default
-    export const replicaRegion = 'us-west-1';
+    export const replicaRegion : string = 'us-west-1';
 
     //if you don't have a Route53 subdomain to use, don't deploy the Tokenizer-DNS
     export const myApiSubdomain : string = "";  //usually 'api'
