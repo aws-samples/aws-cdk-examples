07eb0f7a03adba90352b9a054d1d4434d7d70b2f
EC2 Default User <ec2-user@ip-172-31-39-11.ec2.internal>
2024-06-27 15:05:57 +0000

    removed role

--- python/stackset/service-managed-stackset/cdk_stackset_python/cdk_service_stackset.py
+++ python/stackset/service-managed-stackset/cdk_stackset_python/cdk_service_stackset.py
@@ -16,23 +16,9 @@ class CdkStacksetPythonStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStacksetPythonQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
         stackset_stack = MyStackSet(self, 'MyStackSet')
 
-        admin_role = iam.Role.from_role_arn(
-            self, 'Role', 
-            'arn:aws:iam::123456789:role/AWSCloudFormationStackSetAdministrationRole',
-            mutable=False
-        )
         ou_ids = self.node.try_get_context('OUIDs')
-        print(ou_ids)
         StackSet(self, 'StackSet',
             target=StackSetTarget.from_organizational_units(
                 regions=['us-east-1', 'us-east-2'],
