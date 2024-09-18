aa0a2fa14a35ba85481a939f7d61f577ab56e0fd
Jacky Fan <jacfan@amazon.com>
2024-09-18 13:40:33 +1200

    explicit add account id in the pipeline

--- typescript/aws-codepipeline-ecs-lambda/lib/pipeline-stack.ts
+++ typescript/aws-codepipeline-ecs-lambda/lib/pipeline-stack.ts
@@ -8,14 +8,16 @@ export class pipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const githubOrg       = process.env.GITHUB_ORG       || "aws-6w8hnx";
-    const githubRepo      = process.env.GITHUB_REPO      || "aws-codepipeline-ecs-lambda";
-    const githubBranch    = process.env.GITHUB_BRANCH    || "main";
-    const devEnv          = process.env.DEV_ENV          || "dev";
-    const devAccountId    = process.env.DEV_ACCOUNT_ID   || "117645918752"; // replace with your dev account id
-    const primaryRegion   = process.env.PRIMARY_REGION   || "us-west-2";
-    const secondaryRegion = process.env.SECONDARY_REGION || "eu-west-1";
-
+    const pipelineAccountId = process.env.PIPELINE_ACCOUNT_ID || "111111111111";                 // replace with your pipeline account id
+    const pipelineRegion    = process.env.PIPELINE_REGION     || "us-east-1";                    // replace with your pipeline region
+    const githubOrg         = process.env.GITHUB_ORG          || "aws-6w8hnx";                   // replace with your GitHub Org
+    const githubRepo        = process.env.GITHUB_REPO         || "aws-codepipeline-ecs-lambda";  // replace with your GitHub Repo
+    const githubBranch      = process.env.GITHUB_BRANCH       || "main";                         // replace with your GitHub repo branch
+    const devEnv            = process.env.DEV_ENV             || "dev";                          // replace with your environment
+    const devAccountId      = process.env.DEV_ACCOUNT_ID      || "222222222222";                 // replace with your dev account id
+    const primaryRegion     = process.env.PRIMARY_REGION      || "us-west-2";                    // replace with your primary region
+    const secondaryRegion   = process.env.SECONDARY_REGION    || "eu-west-1";                    // replace with your secondary region
+    
     const pipeline = new CodePipeline(this, 'pipeline', {
       selfMutation:     true,
       crossAccountKeys: true,
@@ -23,7 +25,7 @@ export class pipelineStack extends cdk.Stack {
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection(`${githubOrg}/${githubRepo}`, `${githubBranch}`,{
           // You need to replace the below code connection arn:
-          connectionArn: `arn:aws:codestar-connections:ap-southeast-2:${props?.env?.account}:connection/0ce75950-a29b-4ee4-a9d3-b0bad3b2c0a6`
+          connectionArn: `arn:aws:codestar-connections:${pipelineRegion}:${pipelineAccountId}:connection/0ce75950-a29b-4ee4-a9d3-b0bad3b2c0a6`
         }),
         commands: [
           'npm ci',
@@ -41,7 +43,7 @@ export class pipelineStack extends cdk.Stack {
     });
 
     const devStage = pipeline.addStage(new pipelineAppStage(this, `${devEnv}`, {
-      env: { account: props?.env?.account, region: props?.env?.region}
+      env: { account: `${pipelineAccountId}`, region: `${pipelineRegion}`}
     }));
     devStage.addPost(new ManualApprovalStep('approval'));
 
