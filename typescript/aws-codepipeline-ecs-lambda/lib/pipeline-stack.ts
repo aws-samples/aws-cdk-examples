7cf8ea7bd6398b5ab09237b36c0aaa69b1b791f7
Jacky Fan <jfan@FQs-MBP.local>
2024-10-15 22:32:33 +1300

    moved variables to app.ts

--- typescript/aws-codepipeline-ecs-lambda/lib/pipeline-stack.ts
+++ typescript/aws-codepipeline-ecs-lambda/lib/pipeline-stack.ts
@@ -4,28 +4,30 @@ import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep, Wave }
 import { pipelineAppStage } from './stage-app';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
-export class pipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export interface pipelineProps extends cdk.StackProps {
+  readonly pipelineAccountId: string;
+  readonly pipelineRegion: string;
+  readonly githubOrg: string;
+  readonly githubRepo: string;
+  readonly githubBranch: string;
+  readonly devEnv: string;
+  readonly devAccountId: string;
+  readonly primaryRegion: string;
+  readonly secondaryRegion: string;
+}
 
-    const pipelineAccountId = process.env.PIPELINE_ACCOUNT_ID || "111111111111";                 // replace with your pipeline account id
-    const pipelineRegion    = process.env.PIPELINE_REGION     || "us-east-1";                    // replace with your pipeline region
-    const githubOrg         = process.env.GITHUB_ORG          || "aws-6w8hnx";                   // replace with your GitHub Org
-    const githubRepo        = process.env.GITHUB_REPO         || "aws-codepipeline-ecs-lambda";  // replace with your GitHub Repo
-    const githubBranch      = process.env.GITHUB_BRANCH       || "main";                         // replace with your GitHub repo branch
-    const devEnv            = process.env.DEV_ENV             || "dev";                          // replace with your environment
-    const devAccountId      = process.env.DEV_ACCOUNT_ID      || "222222222222";                 // replace with your dev account id
-    const primaryRegion     = process.env.PRIMARY_REGION      || "us-west-2";                    // replace with your primary region
-    const secondaryRegion   = process.env.SECONDARY_REGION    || "eu-west-1";                    // replace with your secondary region
+export class pipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: pipelineProps) {
+    super(scope, id, props);
     
     const pipeline = new CodePipeline(this, 'pipeline', {
       selfMutation:     true,
       crossAccountKeys: true,
       reuseCrossRegionSupportStacks: true,
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.connection(`${githubOrg}/${githubRepo}`, `${githubBranch}`,{
+        input: CodePipelineSource.connection(`${props.githubOrg}/${props.githubRepo}`, `${props.githubBranch}`,{
           // You need to replace the below code connection arn:
-          connectionArn: `arn:aws:codestar-connections:${pipelineRegion}:${pipelineAccountId}:connection/0ce75950-a29b-4ee4-a9d3-b0bad3b2c0a6`
+          connectionArn: `arn:aws:codestar-connections:${props.pipelineRegion}:${props.pipelineAccountId}:connection/0ce75950-a29b-4ee4-a9d3-b0bad3b2c0a6`
         }),
         commands: [
           'npm ci',
@@ -42,20 +44,20 @@ export class pipelineStack extends cdk.Stack {
       ]}
     });
 
-    const devStage = pipeline.addStage(new pipelineAppStage(this, `${devEnv}`, {
-      env: { account: `${pipelineAccountId}`, region: `${pipelineRegion}`}
+    const devStage = pipeline.addStage(new pipelineAppStage(this, `${props.devEnv}`, {
+      env: { account: `${props.pipelineAccountId}`, region: `${props.pipelineRegion}`}
     }));
     devStage.addPost(new ManualApprovalStep('approval'));
 
     // add waves:
-    const devWave = pipeline.addWave(`${devEnv}Wave`);
+    const devWave = pipeline.addWave(`${props.devEnv}Wave`);
 
-    devWave.addStage(new pipelineAppStage(this, `${devEnv}-${primaryRegion}`, {
-      env: { account: `${devAccountId}`, region: `${primaryRegion}`}
+    devWave.addStage(new pipelineAppStage(this, `${props.devEnv}-${props.primaryRegion}`, {
+      env: { account: `${props.devAccountId}`, region: `${props.primaryRegion}`}
     }));
 
-    devWave.addStage(new pipelineAppStage(this, `${devEnv}-${secondaryRegion}`, {
-      env: { account: `${devAccountId}`, region: `${secondaryRegion}`}
+    devWave.addStage(new pipelineAppStage(this, `${props.devEnv}-${props.secondaryRegion}`, {
+      env: { account: `${props.devAccountId}`, region: `${props.secondaryRegion}`}
     }));
   }
 }
