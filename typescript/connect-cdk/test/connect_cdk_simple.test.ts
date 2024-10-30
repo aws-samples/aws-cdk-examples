8bc04548fa25feeaf0944990f6cebe862c0f67e1
Troy Parrett <parrew@amazon.com>
2024-10-30 15:05:29 -0400

    Simple test

--- typescript/connect-cdk/test/connect_cdk_simple.test.ts
+++ typescript/connect-cdk/test/connect_cdk_simple.test.ts
@@ -1,17 +1,23 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as ConnectCdkSimple from '../lib/connect_cdk_simple-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as ConnectCdk from '../lib/connect_cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/connect_cdk_simple-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ConnectCdkSimple.ConnectCdkSimpleStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('Connect Instance and Lambda Function Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new ConnectCdk.ConnectCdkStack(app, 'MyTestStack');
+  // THEN
+  const template = Template.fromStack(stack);
+
+  // Check that Connect instance is created
+  template.hasResourceProperties('AWS::Connect::Instance', {
+    IdentityManagementType: 'CONNECT_MANAGED'
+  });
+
+  // Check that a Lambda function is created
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Runtime: 'python3.12',
+    Handler: 'lambda_function.lambda_handler'
+  });
 });
