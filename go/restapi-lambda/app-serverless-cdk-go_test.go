e2b4bb9a97de47de6fd9e5c4adccac3bda61ba4c
KovacZan <zan.kovi@proton.me>
2025-03-22 12:33:29 +0100

    fix(go/restapi-lambda): Chnage Runtime, bump go version and bump dependencies

--- go/restapi-lambda/app-serverless-cdk-go_test.go
+++ go/restapi-lambda/app-serverless-cdk-go_test.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"testing"
+  "testing"
 
-	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-	"github.com/aws/jsii-runtime-go"
+  "github.com/aws/aws-cdk-go/awscdk/v2"
+  "github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+  "github.com/aws/jsii-runtime-go"
 )
 
 func TestAppServerlessCdkGoStack(t *testing.T) {
-	// GIVEN
-	app := awscdk.NewApp(nil)
+  // GIVEN
+  app := awscdk.NewApp(nil)
 
-	// WHEN
-	stack := NewAppServerlessCdkGoStack(app, "MyStack", nil)
+  // WHEN
+  stack := NewAppServerlessCdkGoStack(app, "MyStack", nil)
 
-	// THEN
-	template := assertions.Template_FromStack(stack)
+  // THEN
+  template := assertions.Template_FromStack(stack, nil)
 
-	template.HasResourceProperties(jsii.String("AWS::Lambda::Function"), map[string]interface{}{
-		"Runtime": "go1.x",
-	})
+  template.HasResourceProperties(jsii.String("AWS::Lambda::Function"), map[string]interface{}{
+    "Runtime": "provided.al2",
+  })
 
-	template.HasResourceProperties(jsii.String("AWS::ApiGateway::RestApi"), map[string]interface{}{
-		"Name": "myGoApi",
-	})
+  template.HasResourceProperties(jsii.String("AWS::ApiGateway::RestApi"), map[string]interface{}{
+    "Name": "myGoApi",
+  })
 
-	template.HasResourceProperties(jsii.String("AWS::ApiGateway::Resource"), map[string]interface{}{
-		"PathPart": "hello-world",
-	})
+  template.HasResourceProperties(jsii.String("AWS::ApiGateway::Resource"), map[string]interface{}{
+    "PathPart": "hello-world",
+  })
 }
