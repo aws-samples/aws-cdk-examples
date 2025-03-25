46c0a8e90a1f02a774cbfbcd9fe04dc11fc97bb7
Michael Kaiser <kaiser@kaiz.io>
2025-03-25 09:35:27 -0500

    Reset ec2 instance connect

--- typescript/ec2-instance-connect-endpoint/test/integ.default.test.ts
+++ typescript/ec2-instance-connect-endpoint/test/integ.default.test.ts
@@ -1,14 +1,11 @@
 import { Template } from 'aws-cdk-lib/assertions';
 import { IntegTesting } from '../src/integ.default';
-import { normalizeTemplate } from '../../test-utils/normalize-template';
 
 test('default validation', () => {
   const integ = new IntegTesting();
   integ.stack.forEach(stack => {
     const t = Template.fromStack(stack);
-    // Normalize the template before snapshot comparison
-    const normalizedTemplate = normalizeTemplate(t.toJSON());
     // should match snapshot
-    expect(normalizedTemplate).toMatchSnapshot();
+    expect(t).toMatchSnapshot();
   });
 });
